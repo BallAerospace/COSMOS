75783ded4d9c0a86b01bf802b3612713a6cd39f8
Ryan Melton <rmelton@ball.com>
2014-12-29 13:42:33 -0700

    Release COSMOS 3.0.0

--- lib/cosmos/version.rb
+++ lib/cosmos/version.rb
@@ -6,7 +6,7 @@ module Cosmos
     MAJOR = '3'
     MINOR = '0'
     PATCH = '0'
-    BUILD = 'bb4ea81ca18e8efea73a7fcb3816630c16f6a64f'
+    BUILD = '20e7dc437a58774c8698961b73e57632a41d8bd8'
   end
   VERSION = '3.0.0'
 end
