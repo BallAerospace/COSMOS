b48940656c271344ab5d18c7d2361bcb046a9fd4
Ryan Melton <ryanmelt@gmail.com>
2015-01-28 07:39:11 -0700

    Release COSMOS 3.1.0

--- lib/cosmos/version.rb
+++ lib/cosmos/version.rb
@@ -1,12 +1,12 @@
 # encoding: ascii-8bit
 
-COSMOS_VERSION = '3.0.1'
+COSMOS_VERSION = '3.1.0'
 module Cosmos
   module Version
     MAJOR = '3'
-    MINOR = '0'
-    PATCH = '1'
-    BUILD = 'f19556ddbfdda1e439bfaec33e9f81f32a3e868e'
+    MINOR = '1'
+    PATCH = '0'
+    BUILD = 'df3e9ba123a5e75fe76c191e9fab9c80df7ab1d4'
   end
-  VERSION = '3.0.1'
+  VERSION = '3.1.0'
 end
