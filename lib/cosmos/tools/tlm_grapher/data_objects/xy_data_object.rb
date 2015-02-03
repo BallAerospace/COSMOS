9ae601070af885fbd38b882fa839fbce7416b5a5
Jason Thomas <jmthomas@ball.com>
2015-02-03 09:16:34 -0700

    Revert "Remove rescue of all errors to avoid catching real bugs re 37"
    This reverts commit ae655bf09b48835eed798c87e5d91e6e81fcc160.

--- lib/cosmos/tools/tlm_grapher/data_objects/xy_data_object.rb
+++ lib/cosmos/tools/tlm_grapher/data_objects/xy_data_object.rb
@@ -206,10 +206,11 @@ module Cosmos
 
         # Prune Data
         prune_to_max_points_saved()
-      rescue TypeError => error
+      rescue Exception => error
+        raise error if error.class == NoMemoryError
         reset()
+        @error = error
         @plot.redraw_needed = true
-        @error = FatalError.new("Telemetry point #{packet.target_name} #{packet.packet_name} #{@item_name} could not be displayed. This is most likely due to this telemetry point being a String which can't be represented on the graph. Remove the point from the list of telemetry items to cause this exception to go away.\n\n#{error}")
       end
     end # def process_packet
 
