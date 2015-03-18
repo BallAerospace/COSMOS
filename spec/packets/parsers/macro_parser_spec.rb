0091c210950be8b19b828f059fa02c197709b0e8
Ryan Melton <rmelton@ball.com>
2015-03-18 14:40:26 -0600

    #87.  Add bit offset check to macro tests

--- spec/packets/parsers/macro_parser_spec.rb
+++ spec/packets/parsers/macro_parser_spec.rb
@@ -120,9 +120,13 @@ module Cosmos
         expect(pkt.items.length).to eql 7 # 4 plus the RECEIVED_XXX items
         expect(pkt.items.keys).to include('WORD1','WORD2','DOUBLE_WORD1','DOUBLE_WORD2')
         expect(pkt.sorted_items[3].name).to eql 'WORD1'
+        expect(pkt.sorted_items[3].bit_offset).to eql 0
         expect(pkt.sorted_items[4].name).to eql 'DOUBLE_WORD1'
+        expect(pkt.sorted_items[4].bit_offset).to eql 16
         expect(pkt.sorted_items[5].name).to eql 'WORD2'
+        expect(pkt.sorted_items[5].bit_offset).to eql 48
         expect(pkt.sorted_items[6].name).to eql 'DOUBLE_WORD2'
+        expect(pkt.sorted_items[6].bit_offset).to eql 64
         tf.unlink
       end
 
@@ -140,10 +144,15 @@ module Cosmos
         expect(pkt.items.length).to eql 8 # 5 plus the RECEIVED_XXX items
         expect(pkt.items.keys).to include('BIT1','BIT2','BIT3','BIT4','BIT5')
         expect(pkt.sorted_items[3].name).to eql 'BIT1'
+        expect(pkt.sorted_items[3].bit_offset).to eql 0
         expect(pkt.sorted_items[4].name).to eql 'BIT2'
+        expect(pkt.sorted_items[4].bit_offset).to eql 16
         expect(pkt.sorted_items[5].name).to eql 'BIT3'
+        expect(pkt.sorted_items[5].bit_offset).to eql 32
         expect(pkt.sorted_items[6].name).to eql 'BIT4'
+        expect(pkt.sorted_items[6].bit_offset).to eql 48
         expect(pkt.sorted_items[7].name).to eql 'BIT5'
+        expect(pkt.sorted_items[7].bit_offset).to eql 64
         limits_items = []
         pkt.items.each do |name, item|
           limits_items << item if name.include?('BIT')
