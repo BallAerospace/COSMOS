c0017894512a1e0163cf8c0f9fc941eeabac690f
Ryan Melton <rmelton@ball.com>
2015-06-10 10:53:31 -0600

    #144. Reduce duplication in launcher scripts

--- demo/tools/Replay.bat
+++ demo/tools/Replay.bat
@@ -1,39 +1,9 @@
 @ECHO OFF
-SETLOCAL ENABLEEXTENSIONS ENABLEDELAYEDEXPANSION
 
-SET RUBYEXE=rubyw.exe
-
-:: First look two directories up
-SET "DESTINATION_DIR=%~dp0..\..\"
-IF NOT EXIST "!DESTINATION_DIR!Vendor\Ruby" (
-  :: Then check COSMOS_DIR environment variable
-  IF NOT "!COSMOS_DIR!"=="" (
-    SET "DESTINATION_DIR=!COSMOS_DIR!\"
-  )
+IF NOT EXIST LaunchTool.bat (
+  echo LaunchTool.bat does not exist
+  pause
+  exit /b
 )
 
-IF EXIST "!DESTINATION_DIR!Vendor\Ruby" (
-  :: Set environmental variables
-  for /f "delims=" %%a in ('dir "!DESTINATION_DIR!Vendor\Ruby\lib\ruby\gems\2*" /on /ad /b') do set RUBY_ABI=%%a
-  SET "GEM_HOME=!DESTINATION_DIR!Vendor\Ruby\lib\ruby\gems\!RUBY_ABI!"
-  SET "GEM_PATH=!GEM_HOME!"
-  SET "GEMRC=!DESTINATION_DIR!Vendor\Ruby\lib\ruby\gems\etc\gemrc"
-
-  :: Prepend embedded bin to PATH so we prefer those binaries
-  SET "RI_DEVKIT=!DESTINATION_DIR!Vendor\Devkit\"
-  SET "PATH=!DESTINATION_DIR!Vendor\Ruby\bin;!RI_DEVKIT!bin;!RI_DEVKIT!mingw\bin;!DESTINATION_DIR!Vendor\wkhtmltopdf;!PATH!"
-
-  :: Remove RUBYOPT and RUBYLIB, which can cause serious problems.
-  SET RUBYOPT=
-  SET RUBYLIB=
-
-  :: Run tool using Installer Ruby
-  ECHO Starting tool using installer ruby in !DESTINATION_DIR!
-  START "COSMOS" "!DESTINATION_DIR!Vendor\Ruby\bin\!RUBYEXE!" "%~dp0%~n0" %*
-) else (
-  :: Use System Ruby and Environment
-  ECHO Starting tool using system ruby and environment
-  START "COSMOS" "!RUBYEXE!" "%~dp0%~n0" %*
-)
-
-ENDLOCAL
+call LaunchTool.bat rubyw.exe %~n0 %*
