5a781844b47e5744c44eeb8938efc36dac1c1a31
Jason Thomas <jmthomas@ball.com>
2015-07-08 11:07:23 -0600

    Fix install/Launcher.bat

--- install/Launcher.bat
+++ install/Launcher.bat
@@ -1,9 +1,9 @@
 @ECHO OFF
 
-IF NOT EXIST %~dp0tools\LaunchTool.bat (
-  echo %~dp0tools\LaunchTool.bat does not exist
+IF NOT EXIST %~dp0tools\ToolLaunch.bat (
+  echo %~dp0tools\ToolLaunch.bat does not exist
   pause
   exit /b
 )
 
-call %~dp0tools\LaunchTool.bat rubyw.exe %~n0 %*
+call %~dp0tools\ToolLaunch.bat rubyw.exe %~n0 %*
