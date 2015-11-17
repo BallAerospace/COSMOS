81cc9a9cc50d26023495f42044cb7a7a5c694b26
Jason Thomas <jmthomas@ball.com>
2015-11-17 10:40:12 -0700

    Create common limits_widget.rb base class

--- lib/cosmos/tools/tlm_viewer/widgets/limitsbar_widget.rb
+++ lib/cosmos/tools/tlm_viewer/widgets/limitsbar_widget.rb
@@ -8,170 +8,100 @@
 # as published by the Free Software Foundation; version 3 with
 # attribution addendums as found in the LICENSE.txt
 
-require 'cosmos/tools/tlm_viewer/widgets/widget'
+require 'cosmos/tools/tlm_viewer/widgets/limits_widget'
 
 module Cosmos
 
-  class LimitsbarWidget < Qt::Label
-    include Widget
+  class LimitsbarWidget < LimitsWidget
 
     def initialize(parent_layout, target_name, packet_name, item_name, value_type = :CONVERTED, width = 160, height = 25)
-      super(target_name, packet_name, item_name, value_type)
-      @value_type = :CONVERTED if @value_type == :WITH_UNITS
-      @width = width.to_i
-      @height = height.to_i
-      @value = 0
-      @x_pad = 6
-      @y_pad = 6
-      @bar_width = @width - (2 * @x_pad)
-      @bar_height = @height - (2 * @y_pad)
-      @painter = nil
-      setFixedSize(width.to_i, height.to_i)
-      parent_layout.addWidget(self) if parent_layout
+      super(parent_layout, target_name, packet_name, item_name, value_type, width, height)
     end
 
     def LimitsbarWidget.takes_value?
       return true
     end
 
-    def value=(data)
-      @value = data.to_f
-      update()
-    end
-
-    def paintEvent(event)
-      begin
-        return if @painter
-        @painter = Qt::Painter.new(self)
-        # Seems like on initialization sometimes we get some weird bad conditions so check for them
-        if @painter.isActive and @painter.paintEngine
-          paint_implementation(@painter)
-        end
-        @painter.dispose
-        @painter = nil
-      rescue Exception => err
-        Cosmos.handle_fatal_exception(err)
-      end
-    end
-
     def paint_implementation(dc)
-      draw_bar = true
+      limits = get_limits()
+      return unless limits
 
-      limits_values = @item.limits.values
-      if limits_values
-        limits = limits_values[@limits_set]
-        limits = limits_values[:DEFAULT] unless limits
-        if limits
-          red_low = limits[0]
-          yellow_low = limits[1]
-          yellow_high = limits[2]
-          red_high = limits[3]
-          green_low = limits[4]
-          green_high = limits[5]
-        else
-          draw_bar = false
-        end
+      widths = calculate_widths(limits, @bar_width)
+
+      # Set starting points
+      x_pos = @x_pad
+      y_pos = @y_pad
+
+      # Draw RED_LOW bar
+      dc.addRectColorFill(x_pos, y_pos, widths.red_low, @bar_height, 'red')
+      dc.addRectColor(x_pos, y_pos, widths.red_low, @bar_height)
+      x_pos += widths.red_low
+
+      # Draw YELLOW_LOW bar
+      dc.addRectColorFill(x_pos, y_pos, widths.yellow_low, @bar_height, 'yellow')
+      dc.addRectColor(x_pos, y_pos, widths.yellow_low, @bar_height)
+      x_pos += widths.yellow_low
+
+      if widths.green_high
+        # Draw GREEN_LOW bar
+        dc.addRectColorFill(x_pos, y_pos, widths.green_low, @bar_height, 'lime')
+        dc.addRectColor(x_pos, y_pos, widths.green_low, @bar_height)
+        x_pos += widths.green_low
+
+        # Draw BLUE bar
+        dc.addRectColorFill(x_pos, y_pos, widths.blue, @bar_height, 'dodgerblue')
+        dc.addRectColor(x_pos, y_pos, widths.blue, @bar_height)
+        x_pos += widths.blue
+
+        # Draw GREEN_HIGH bar
+        dc.addRectColorFill(x_pos, y_pos, widths.green_high, @bar_height, 'lime')
+        dc.addRectColor(x_pos, y_pos, widths.green_high, @bar_height)
+        x_pos += widths.green_high
       else
-        draw_bar = false
+        # Draw GREEN bar
+        dc.addRectColorFill(x_pos, y_pos, widths.green, @bar_height, 'lime')
+        dc.addRectColor(x_pos, y_pos, widths.green, @bar_height)
+        x_pos += widths.green
       end
 
-      if draw_bar
-        # Calculate sizes of limits sections
-        red_low_width  = (0.1 * @bar_width).round
-        red_high_width = (0.1 * @bar_width).round
+      # Draw YELLOW_HIGH bar
+      dc.addRectColorFill(x_pos, y_pos, widths.yellow_high, @bar_height, 'yellow')
+      dc.addRectColor(x_pos, y_pos, widths.yellow_high, @bar_height)
+      x_pos += widths.yellow_high
 
-        inner_value_range = red_high - red_low
+      # Draw RED_HIGH bar
+      dc.addRectColorFill(x_pos, y_pos, widths.red_high, @bar_height, 'red')
+      dc.addRectColor(x_pos, y_pos, widths.red_high, @bar_height)
+      x_pos += widths.red_high
 
-        yellow_low_width  = ((yellow_low - red_low)   / inner_value_range * 0.8 * @bar_width).round
-        yellow_high_width = ((red_high - yellow_high) / inner_value_range * 0.8 * @bar_width).round
+      # Draw line at current value
+      red_low = limits[0]
+      red_high = limits[3]
+      @bar_scale = (red_high - red_low) / 0.8
+      @low_value = red_low - 0.1 * @bar_scale
+      @high_value = red_high + 0.1 * @bar_scale
 
-        if green_high
-          green_low_width  = ((green_low - yellow_low)   / inner_value_range * 0.8 * @bar_width).round
-          green_high_width = ((yellow_high - green_high) / inner_value_range * 0.8 * @bar_width).round
-          blue_width = @bar_width - red_low_width - yellow_low_width - green_low_width - green_high_width - yellow_high_width - red_high_width
-        else
-          green_width = @bar_width - red_low_width - yellow_low_width - yellow_high_width - red_high_width
-        end
+      @line_pos = (@x_pad + (@value - @low_value) / @bar_scale * @bar_width).to_i
+      if @line_pos < @x_pad
+        @line_pos = @x_pad
+      end
+      if @line_pos > @x_pad + @bar_width
+        @line_pos = @bar_width + @x_pad
+      end
 
-        # Set starting points
-        x_pos = @x_pad
-        y_pos = @y_pad
+      dc.addLineColor(@line_pos, @y_pad - 3, @line_pos, @y_pad + @bar_height + 3)
 
-        # Draw RED_LOW bar
-        dc.addRectColorFill(x_pos, y_pos, red_low_width, @bar_height, 'red')
-        dc.addRectColor(x_pos, y_pos, red_low_width, @bar_height)
-        x_pos += red_low_width
+      # Draw triangle above current value line
+      top_triangle = Qt::Polygon.new(3)
+      top_triangle.setPoint(0, @line_pos, @y_pad - 1)
+      top_triangle.setPoint(1, @line_pos-5, @y_pad - 6)
+      top_triangle.setPoint(2, @line_pos+5, @y_pad - 6)
+      dc.setBrush(Cosmos::BLACK)
+      dc.drawPolygon(top_triangle)
+      top_triangle.dispose
 
-        # Draw YELLOW_LOW bar
-        dc.addRectColorFill(x_pos, y_pos, yellow_low_width, @bar_height, 'yellow')
-        dc.addRectColor(x_pos, y_pos, yellow_low_width, @bar_height)
-        x_pos += yellow_low_width
-
-        if green_high
-          # Draw GREEN_LOW bar
-          dc.addRectColorFill(x_pos, y_pos, green_low_width, @bar_height, 'lime')
-          dc.addRectColor(x_pos, y_pos, green_low_width, @bar_height)
-          x_pos += green_low_width
-
-          # Draw BLUE bar
-          dc.addRectColorFill(x_pos, y_pos, blue_width, @bar_height, 'dodgerblue')
-          dc.addRectColor(x_pos, y_pos, blue_width, @bar_height)
-          x_pos += blue_width
-
-          # Draw GREEN_HIGH bar
-          dc.addRectColorFill(x_pos, y_pos, green_high_width, @bar_height, 'lime')
-          dc.addRectColor(x_pos, y_pos, green_high_width, @bar_height)
-          x_pos += green_high_width
-        else
-          # Draw GREEN bar
-          dc.addRectColorFill(x_pos, y_pos, green_width, @bar_height, 'lime')
-          dc.addRectColor(x_pos, y_pos, green_width, @bar_height)
-          x_pos += green_width
-        end
-
-        # Draw YELLOW_HIGH bar
-        dc.addRectColorFill(x_pos, y_pos, yellow_high_width, @bar_height, 'yellow')
-        dc.addRectColor(x_pos, y_pos, yellow_high_width, @bar_height)
-        x_pos += yellow_high_width
-
-        # Draw RED_HIGH bar
-        dc.addRectColorFill(x_pos, y_pos, red_high_width, @bar_height, 'red')
-        dc.addRectColor(x_pos, y_pos, red_high_width, @bar_height)
-        x_pos += red_high_width
-
-        # Draw line at current value
-        @bar_scale = (red_high - red_low) / 0.8
-        @low_value = red_low - 0.1 * @bar_scale
-        @high_value = red_high + 0.1 * @bar_scale
-
-        @line_pos = (@x_pad + (@value - @low_value) / @bar_scale * @bar_width).to_i
-        if @line_pos < @x_pad
-          @line_pos = @x_pad
-        end
-        if @line_pos > @x_pad + @bar_width
-          @line_pos = @bar_width + @x_pad
-        end
-
-        dc.addLineColor(@line_pos, @y_pad - 3, @line_pos, @y_pad + @bar_height + 3)
-
-        # Draw triangle above current value line
-        top_triangle = Qt::Polygon.new(3)
-        top_triangle.setPoint(0, @line_pos, @y_pad - 1)
-        top_triangle.setPoint(1, @line_pos-5, @y_pad - 6)
-        top_triangle.setPoint(2, @line_pos+5, @y_pad - 6)
-        dc.setBrush(Cosmos::BLACK)
-        dc.drawPolygon(top_triangle)
-        top_triangle.dispose
-
-        # Additional drawing for subclasses
-        additional_drawing(dc)
-      end # if draw_bar == true
-    end
-
-    protected
-
-    def additional_drawing(dc)
-      # Do nothing
+      # Additional drawing for subclasses
+      additional_drawing(dc)
     end
   end
 
--- lib/cosmos/tools/tlm_viewer/widgets/limitscolumn_widget.rb
+++ lib/cosmos/tools/tlm_viewer/widgets/limitscolumn_widget.rb
@@ -8,170 +8,100 @@
 # as published by the Free Software Foundation; version 3 with
 # attribution addendums as found in the LICENSE.txt
 
-require 'cosmos/tools/tlm_viewer/widgets/widget'
+require 'cosmos/tools/tlm_viewer/widgets/limits_widget'
 
 module Cosmos
 
-  class LimitscolumnWidget < Qt::Label
-    include Widget
+  class LimitscolumnWidget < LimitsWidget
 
     def initialize(parent_layout, target_name, packet_name, item_name, value_type = :CONVERTED, width = 30, height = 100)
-      super(target_name, packet_name, item_name, value_type)
-      @value_type = :CONVERTED if @value_type == :WITH_UNITS
-      @width = width.to_i
-      @height = height.to_i
-      @value = 0
-      @x_pad = 6
-      @y_pad = 6
-      @bar_width = @width - (2 * @x_pad)
-      @bar_height = @height - (2 * @y_pad)
-      @painter = nil
-      setFixedSize(width.to_i, height.to_i)
-      parent_layout.addWidget(self) if parent_layout
+      super(parent_layout, target_name, packet_name, item_name, value_type, width, height)
     end
 
     def LimitscolumnWidget.takes_value?
       return true
     end
 
-    def value=(data)
-      @value = data.to_f
-      update()
-    end
-
-    def paintEvent(event)
-      begin
-        return if @painter
-        @painter = Qt::Painter.new(self)
-        # Seems like on initialization sometimes we get some weird bad conditions so check for them
-        if @painter.isActive and @painter.paintEngine
-          paint_implementation(@painter)
-        end
-        @painter.dispose
-        @painter = nil
-      rescue Exception => err
-        Cosmos.handle_fatal_exception(err)
-      end
-    end
-
     def paint_implementation(dc)
-      draw_bar = true
+      limits = get_limits()
+      return unless limits
 
-      limits_values = @item.limits.values
-      if limits_values
-        limits = limits_values[@limits_set]
-        limits = limits_values[:DEFAULT] unless limits
-        if limits
-          red_low = limits[0]
-          yellow_low = limits[1]
-          yellow_high = limits[2]
-          red_high = limits[3]
-          green_low = limits[4]
-          green_high = limits[5]
-        else
-          draw_bar = false
-        end
+      widths = calculate_widths(limits, @bar_height)
+
+      # Set starting points
+      x_pos = @x_pad
+      y_pos = @y_pad
+
+      # Draw RED_LOW bar
+      dc.addRectColorFill(x_pos, y_pos, @bar_width, widths.red_low, 'red')
+      dc.addRectColor(x_pos, y_pos, @bar_width, widths.red_low)
+      y_pos += widths.red_low
+
+      # Draw YELLOW_LOW bar
+      dc.addRectColorFill(x_pos, y_pos, @bar_width, widths.yellow_low, 'yellow')
+      dc.addRectColor(x_pos, y_pos, @bar_width, widths.yellow_low)
+      y_pos += widths.yellow_low
+
+      if widths.green_high
+        # Draw GREEN_LOW bar
+        dc.addRectColorFill(x_pos, y_pos, @bar_width, widths.green_low, 'lime')
+        dc.addRectColor(x_pos, y_pos, @bar_width, widths.green_low)
+        y_pos += widths.green_low
+
+        # Draw BLUE bar
+        dc.addRectColorFill(x_pos, y_pos, @bar_width, widths.blue, 'dodgerblue')
+        dc.addRectColor(x_pos, y_pos, @bar_width, widths.blue)
+        y_pos += widths.blue
+
+        # Draw GREEN_HIGH bar
+        dc.addRectColorFill(x_pos, y_pos, @bar_width, widths.green_high, 'lime')
+        dc.addRectColor(x_pos, y_pos, @bar_width, widths.green_high)
+        y_pos += widths.green_high
       else
-        draw_bar = false
+        # Draw GREEN bar
+        dc.addRectColorFill(x_pos, y_pos, @bar_width, widths.green, 'lime')
+        dc.addRectColor(x_pos, y_pos, @bar_width, widths.green)
+        y_pos += widths.green
       end
 
-      if draw_bar
-        # Calculate sizes of limits sections
-        red_low_height  = (0.1 * @bar_height).round
-        red_high_height = (0.1 * @bar_height).round
+      # Draw YELLOW_HIGH bar
+      dc.addRectColorFill(x_pos, y_pos, @bar_width, widths.yellow_high, 'yellow')
+      dc.addRectColor(x_pos, y_pos, @bar_width, widths.yellow_high)
+      y_pos += widths.yellow_high
 
-        inner_value_range = red_high - red_low
+      # Draw RED_HIGH bar
+      dc.addRectColorFill(x_pos, y_pos, @bar_width, widths.red_high, 'red')
+      dc.addRectColor(x_pos, y_pos, @bar_width, widths.red_high)
+      y_pos += widths.red_high
 
-        yellow_low_height  = ((yellow_low - red_low)   / inner_value_range * 0.8 * @bar_height).round
-        yellow_high_height = ((red_high - yellow_high) / inner_value_range * 0.8 * @bar_height).round
+      # Draw line at current value
+      red_low = limits[0]
+      red_high = limits[3]
+      @bar_scale = (red_high - red_low) / 0.8
+      @low_value = red_low - 0.1 * @bar_scale
+      @high_value = red_high + 0.1 * @bar_scale
 
-        if green_high
-          green_low_height  = ((green_low - yellow_low)   / inner_value_range * 0.8 * @bar_height).round
-          green_high_height = ((yellow_high - green_high) / inner_value_range * 0.8 * @bar_height).round
-          blue_height = @bar_height - red_low_height - yellow_low_height - green_low_height - green_high_height - yellow_high_height - red_high_height
-        else
-          green_height = @bar_height - red_low_height - yellow_low_height - yellow_high_height - red_high_height
-        end
+      @line_pos = @height - (@y_pad + (@value - @low_value) / @bar_scale * @bar_height).to_i
+      if @line_pos < @y_pad
+        @line_pos = @y_pad
+      end
+      if @line_pos > @y_pad + @bar_height
+        @line_pos = @bar_height + @y_pad
+      end
 
-        # Set starting points
-        x_pos = @x_pad
-        y_pos = @y_pad
+      dc.addLineColor(@x_pad - 3, @line_pos, @x_pad + @bar_width + 3, @line_pos)
 
-        # Draw RED_LOW bar
-        dc.addRectColorFill(x_pos, y_pos, @bar_width, red_low_height, 'red')
-        dc.addRectColor(x_pos, y_pos, @bar_width, red_low_height)
-        y_pos += red_low_height
+      # Draw triangle next to current value line
+      top_triangle = Qt::Polygon.new(3)
+      top_triangle.setPoint(0, @x_pad + @bar_width, @line_pos)
+      top_triangle.setPoint(1, @x_pad + @bar_width + 5, @line_pos + 5)
+      top_triangle.setPoint(2, @x_pad + @bar_width + 5, @line_pos - 5)
+      dc.setBrush(Cosmos::BLACK)
+      dc.drawPolygon(top_triangle)
+      top_triangle.dispose
 
-        # Draw YELLOW_LOW bar
-        dc.addRectColorFill(x_pos, y_pos, @bar_width, yellow_low_height, 'yellow')
-        dc.addRectColor(x_pos, y_pos, @bar_width, yellow_low_height)
-        y_pos += yellow_low_height
-
-        if green_high
-          # Draw GREEN_LOW bar
-          dc.addRectColorFill(x_pos, y_pos, @bar_width, green_low_height, 'lime')
-          dc.addRectColor(x_pos, y_pos, @bar_width, green_low_height)
-          y_pos += green_low_height
-
-          # Draw BLUE bar
-          dc.addRectColorFill(x_pos, y_pos, @bar_width, blue_height, 'dodgerblue')
-          dc.addRectColor(x_pos, y_pos, @bar_width, blue_height)
-          y_pos += blue_height
-
-          # Draw GREEN_HIGH bar
-          dc.addRectColorFill(x_pos, y_pos, @bar_width, green_high_height, 'lime')
-          dc.addRectColor(x_pos, y_pos, @bar_width, green_high_height)
-          y_pos += green_high_height
-        else
-          # Draw GREEN bar
-          dc.addRectColorFill(x_pos, y_pos, @bar_width, green_height, 'lime')
-          dc.addRectColor(x_pos, y_pos, @bar_width, green_height)
-          y_pos += green_height
-        end
-
-        # Draw YELLOW_HIGH bar
-        dc.addRectColorFill(x_pos, y_pos, @bar_width, yellow_high_height, 'yellow')
-        dc.addRectColor(x_pos, y_pos, @bar_width, yellow_high_height)
-        y_pos += yellow_high_height
-
-        # Draw RED_HIGH bar
-        dc.addRectColorFill(x_pos, y_pos, @bar_width, red_high_height, 'red')
-        dc.addRectColor(x_pos, y_pos, @bar_width, red_high_height)
-        y_pos += red_high_height
-
-        # Draw line at current value
-        @bar_scale = (red_high - red_low) / 0.8
-        @low_value = red_low - 0.1 * @bar_scale
-        @high_value = red_high + 0.1 * @bar_scale
-
-        @line_pos = @height - (@y_pad + (@value - @low_value) / @bar_scale * @bar_height).to_i
-        if @line_pos < @y_pad
-          @line_pos = @y_pad
-        end
-        if @line_pos > @y_pad + @bar_height
-          @line_pos = @bar_height + @y_pad
-        end
-
-        dc.addLineColor(@x_pad - 3, @line_pos, @x_pad + @bar_width + 3, @line_pos)
-
-        # Draw triangle next to current value line
-        top_triangle = Qt::Polygon.new(3)
-        top_triangle.setPoint(0, @x_pad + @bar_width, @line_pos)
-        top_triangle.setPoint(1, @x_pad + @bar_width + 5, @line_pos + 5)
-        top_triangle.setPoint(2, @x_pad + @bar_width + 5, @line_pos - 5)
-        dc.setBrush(Cosmos::BLACK)
-        dc.drawPolygon(top_triangle)
-        top_triangle.dispose
-
-        # Additional drawing for subclasses
-        additional_drawing(dc)
-      end # if draw_bar == true
-    end
-
-    protected
-
-    def additional_drawing(dc)
-      # Do nothing
+      # Additional drawing for subclasses
+      additional_drawing(dc)
     end
   end
 
