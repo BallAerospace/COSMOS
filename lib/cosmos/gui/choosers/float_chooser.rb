90f319a1edab5da51a1c8950c23135402cf03ef1
Jason Thomas <jmthomas@ball.com>
2015-12-29 13:25:42 -0700

    Refactor choosers into ValueChooser base class.

--- lib/cosmos/gui/choosers/float_chooser.rb
+++ lib/cosmos/gui/choosers/float_chooser.rb
@@ -9,6 +9,7 @@
 # attribution addendums as found in the LICENSE.txt
 
 require 'cosmos'
+require 'cosmos/gui/choosers/value_chooser'
 
 module Cosmos
 
@@ -33,71 +34,32 @@ module Cosmos
     end
   end
 
-  class FloatChooser < Qt::Widget
-
+  class FloatChooser < ValueChooser
     # Callback for a new value entered into the text field
     attr_accessor :sel_command_callback
 
-    def initialize(
-      parent, label_text, initial_value,
-      minimum_value = nil, maximum_value = nil, field_width = 20, fill = false
-    )
-      super(parent)
+    def initialize(parent, label_text, initial_value,
+                   minimum_value = nil, maximum_value = nil,
+                   field_width = 20, fill = false)
+      super(parent, label_text, initial_value, field_width, fill)
       @minimum_value = minimum_value
       @maximum_value = maximum_value
-      @field_width   = field_width
 
-      layout = Qt::HBoxLayout.new(self)
-      layout.setContentsMargins(0,0,0,0)
-
-      @float_label = Qt::Label.new(label_text)
-      @float_label.setSizePolicy(Qt::SizePolicy::Fixed, Qt::SizePolicy::Fixed) if fill
-      layout.addWidget(@float_label)
-      layout.addStretch unless fill
-      @float_value = Qt::LineEdit.new(initial_value.to_s)
-      @float_value.setMinimumWidth(field_width)
-      if minimum_value or maximum_value
-        validator = FloatChooserDoubleValidator.new(@float_value)
-        validator.setBottom(minimum_value) if minimum_value
-        validator.setTop(maximum_value) if maximum_value
-        validator.setNotation(Qt::DoubleValidator::StandardNotation)
-        @float_value.setValidator(validator)
-      end
-      @callback_in_progress = false
-      @float_value.connect(SIGNAL('editingFinished()')) do
-        unless @callback_in_progress # Prevent double fire on loss of focus
-          begin
-            @callback_in_progress = true
-            @sel_command_callback.call(string(), value()) if @sel_command_callback
-          ensure
-            @callback_in_progress = false
-          end
-        end
-      end
-      layout.addWidget(@float_value)
-      setLayout(layout)
-
-      @sel_command_callback = nil
-    end
-
-    # Returns the value as a string
-    def string
-      @float_value.text
+      validator = FloatChooserDoubleValidator.new(@value)
+      validator.setBottom(minimum_value) if minimum_value
+      validator.setTop(maximum_value) if maximum_value
+      validator.setNotation(Qt::DoubleValidator::StandardNotation)
+      @value.setValidator(validator)
     end
 
     # Returns the value as a float
     def value
-      float_value = @float_value.text.to_f
-      float_value = @minimum_value if @minimum_value and float_value < @minimum_value
-      float_value = @maximum_value if @maximum_value and float_value > @maximum_value
+      float_value = @value.text.to_f
+      float_value = @minimum_value if @minimum_value && float_value < @minimum_value
+      float_value = @maximum_value if @maximum_value && float_value > @maximum_value
       float_value
     end
 
-    # Sets the value of the text field
-    def value=(new_value)
-      @float_value.setText(new_value.to_s)
-    end
+  end
+end
 
-  end # class FloatChooser
-
-end # module Cosmos
--- lib/cosmos/gui/choosers/integer_chooser.rb
+++ lib/cosmos/gui/choosers/integer_chooser.rb
@@ -9,6 +9,7 @@
 # attribution addendums as found in the LICENSE.txt
 
 require 'cosmos'
+require 'cosmos/gui/choosers/value_chooser'
 
 module Cosmos
 
@@ -36,69 +37,31 @@ module Cosmos
     end
   end
 
-  class IntegerChooser < Qt::Widget
-
+  class IntegerChooser < ValueChooser
     # Callback called when the value changes
     attr_accessor :sel_command_callback
 
-    def initialize(
-      parent, label_text, initial_value,
-      minimum_value = nil, maximum_value = nil, field_width = 20, fill = false
-    )
-      super(parent)
+    def initialize(parent, label_text, initial_value,
+                   minimum_value = nil, maximum_value = nil,
+                   field_width = 20, fill = false)
+      super(parent, label_text, initial_value, field_width, fill)
       @minimum_value = minimum_value
       @maximum_value = maximum_value
 
-      layout = Qt::HBoxLayout.new(self)
-      layout.setContentsMargins(0,0,0,0)
-
-      @integer_label = Qt::Label.new(label_text)
-      @integer_label.setSizePolicy(Qt::SizePolicy::Fixed, Qt::SizePolicy::Fixed) if fill
-      layout.addWidget(@integer_label)
-      layout.addStretch() unless fill
-      @integer_value = Qt::LineEdit.new(initial_value.to_s)
-      @integer_value.setMinimumWidth(field_width)
-      if minimum_value or maximum_value
-        validator = IntegerChooserIntValidator.new(@integer_value)
-        validator.setBottom(minimum_value) if minimum_value
-        validator.setTop(maximum_value) if maximum_value
-        @integer_value.setValidator(validator)
-      end
-      @callback_in_progress = false
-      @integer_value.connect(SIGNAL('editingFinished()')) do
-        unless @callback_in_progress # Prevent double fire on loss of focus
-          begin
-            @callback_in_progress = true
-            @sel_command_callback.call(string(), value()) if @sel_command_callback
-          ensure
-            @callback_in_progress = false
-          end
-        end
-      end
-      layout.addWidget(@integer_value)
-      setLayout(layout)
-
-      @sel_command_callback = nil
-    end
-
-    # Returns the value as a string
-    def string
-      @integer_value.text
+      validator = IntegerChooserIntValidator.new(@value)
+      validator.setBottom(minimum_value) if minimum_value
+      validator.setTop(maximum_value) if maximum_value
+      @value.setValidator(validator)
     end
 
     # Returns the value as an integer
     def value
-      integer_value = @integer_value.text.to_i
-      integer_value = @minimum_value if @minimum_value and integer_value < @minimum_value
-      integer_value = @maximum_value if @maximum_value and integer_value > @maximum_value
+      integer_value = @value.text.to_i
+      integer_value = @minimum_value if @minimum_value && integer_value < @minimum_value
+      integer_value = @maximum_value if @maximum_value && integer_value > @maximum_value
       integer_value
     end
 
-    # Sets the value
-    def value=(new_value)
-      @integer_value.setText(new_value.to_s)
-    end
+  end
+end
 
-  end # class IntegerChooser
-
-end # module Cosmos
