a8802e052821bb985a54684f3d8be0f4decb4557
Cody Planteen <planteen@gmail.com>
2016-02-16 09:20:27 -0700

    Add support for getting the buffer in get_cmd_value(), add virtual parameter of INTERFACE_BUFFER

--- lib/cosmos/tools/cmd_tlm_server/api.rb
+++ lib/cosmos/tools/cmd_tlm_server/api.rb
@@ -286,7 +286,8 @@ module Cosmos
     # @return [Varies] value
     def get_cmd_value(target_name, command_name, parameter_name, value_type = :CONVERTED)
       packet = System.commands.packet(target_name, command_name)
-      # Virtually support RECEIVED_TIMEFORMATTED, RECEIVED_TIMESECONDS, RECEIVED_COUNT
+      # Virtually support RECEIVED_TIMEFORMATTED, RECEIVED_TIMESECONDS,
+      # RECEIVED_COUNT, INTERFACE_BUFFER
       case parameter_name.to_s.upcase
       when 'RECEIVED_TIMEFORMATTED'
         if packet.received_time
@@ -302,6 +303,8 @@ module Cosmos
         end
       when 'RECEIVED_COUNT'
         return packet.received_count
+      when 'INTERFACE_BUFFER'
+        return packet.buffer
       else
         return packet.read(parameter_name, value_type.intern)
       end
