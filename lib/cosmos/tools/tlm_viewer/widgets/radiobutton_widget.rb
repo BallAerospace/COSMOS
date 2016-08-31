f05cca129e040be148736d8923794b59478c0cd4
Jason Thomas <jmthomas@ball.com>
2016-08-31 17:19:21 -0600

    Only allow CHECKED and UNCHECKED

--- lib/cosmos/tools/tlm_viewer/widgets/radiobutton_widget.rb
+++ lib/cosmos/tools/tlm_viewer/widgets/radiobutton_widget.rb
@@ -16,10 +16,17 @@ module Cosmos
   class RadiobuttonWidget < Qt::RadioButton
     include Widget
 
-    def initialize(parent_layout, radiobutton_text, checked = false)
+    def initialize(parent_layout, radiobutton_text, checked = 'UNCHECKED')
       super()
       setText(radiobutton_text.to_s)
-      setChecked(true) if checked
+      case checked.to_s
+      when 'CHECKED'
+        setChecked(true)
+      when 'UNCHECKED'
+        setChecked(false)
+      else
+        raise "Unknown option '#{checked}' given to #{self.class}"
+      end
       parent_layout.addWidget(self) if parent_layout
     end
 
