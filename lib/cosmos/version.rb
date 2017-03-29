e5fecad148c4a9bb0628de4ce3026b1ee65e9a5a
= <=>
2017-03-29 15:59:02 -0600

    Release COSMOS 3.9.1

--- lib/cosmos/version.rb
+++ lib/cosmos/version.rb
@@ -1,12 +1,12 @@
 # encoding: ascii-8bit
 
-COSMOS_VERSION = '3.9.0'
+COSMOS_VERSION = '3.9.1'
 module Cosmos
   module Version
     MAJOR = '3'
     MINOR = '9'
-    PATCH = '0'
-    BUILD = '45799e4f04a4c4a9dc463b044de22a6978723120'
+    PATCH = '1'
+    BUILD = '0d2d495007138d6145a42b4cba9176f3e606640d'
   end
-  VERSION = '3.9.0'
+  VERSION = '3.9.1'
 end
