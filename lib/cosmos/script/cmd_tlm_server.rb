61aad646e1b6b0ff05ae7b9117b2f49b22d0fa8b
Donald Hall <dhall@ball.com>
2017-10-26 17:38:13 -0600

    re #626 Add API functions to get ignored parameters and items

--- lib/cosmos/script/cmd_tlm_server.rb
+++ lib/cosmos/script/cmd_tlm_server.rb
@@ -61,6 +61,14 @@ module Cosmos
       return $cmd_tlm_server.get_all_target_info
     end
 
+    def get_target_ignored_parameters(target_name)
+      return $cmd_tlm_server.get_target_ignored_parameters(target_name)
+    end
+
+    def get_target_ignored_items(target_name)
+      return $cmd_tlm_server.get_target_ignored_items(target_name)
+    end
+
     def get_interface_info(interface_name)
       return $cmd_tlm_server.get_interface_info(interface_name)
     end
