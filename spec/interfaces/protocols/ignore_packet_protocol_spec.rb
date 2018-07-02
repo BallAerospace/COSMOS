f740e15e8f04e7bcc7ae012b1b76f7f4af96d9ca
Jason Thomas <jmthomas@ball.com>
2018-07-02 16:43:18 -0600

    Add IgnorePacketProtocol

---
+++ spec/interfaces/protocols/ignore_packet_protocol_spec.rb
@@ -0,0 +1,136 @@
+# encoding: ascii-8bit
+
+# Copyright 2014 Ball Aerospace & Technologies Corp.
+# All Rights Reserved.
+#
+# This program is free software; you can modify and/or redistribute it
+# under the terms of the GNU General Public License
+# as published by the Free Software Foundation; version 3 with
+# attribution addendums as found in the LICENSE.txt
+
+require 'spec_helper'
+require 'cosmos/interfaces/protocols/ignore_packet_protocol'
+require 'cosmos/interfaces/interface'
+require 'cosmos/streams/stream'
+
+module Cosmos
+  describe IgnorePacketProtocol do
+    before(:each) do
+      $buffer = ''
+      @interface = StreamInterface.new
+      @interface.target_names = ['SYSTEM']
+      allow(@interface).to receive(:connected?) { true }
+    end
+
+    class PreStream < Stream
+      def connect; end
+      def connected?; true; end
+      def disconnect; end
+      def read; $buffer; end
+      def write(data); $buffer = data; end
+    end
+
+    describe "initialize" do
+      it "complains if target is not given" do
+        expect { @interface.add_protocol(IgnorePacketProtocol, [], :READ_WRITE) }.to raise_error(ArgumentError)
+      end
+
+      it "complains if packet is not given" do
+        expect { @interface.add_protocol(IgnorePacketProtocol, ['SYSTEM'], :READ_WRITE) }.to raise_error(ArgumentError)
+      end
+
+      it "complains if the target is not found" do
+        expect { @interface.add_protocol(IgnorePacketProtocol, ['BLAH', 'META'], :READ_WRITE) }.to raise_error(/target 'BLAH' does not exist/)
+      end
+
+      it "complains if the target is not found" do
+        expect { @interface.add_protocol(IgnorePacketProtocol, ['SYSTEM', 'BLAH'], :READ_WRITE) }.to raise_error(/packet 'SYSTEM BLAH' does not exist/)
+      end
+    end
+
+    describe "read" do
+      it "ignores the packet specified" do
+        @interface.instance_variable_set(:@stream, PreStream.new)
+        @interface.add_protocol(IgnorePacketProtocol, ['SYSTEM', 'META'], :READ)
+        pkt = System.telemetry.packet("SYSTEM","META")
+        pkt.write("COSMOS_VERSION", "TEST")
+        time = Time.new(2020,1,31,12,15,30.5)
+        pkt.received_time = time
+        $buffer = ''
+        @interface.write(pkt)
+        # Verify the write went out
+        expect(pkt.buffer).to eql $buffer
+
+        packet = nil
+        # Try to read the interface
+        # We put this in a thread because it calls it continuously
+        thread = Thread.new do
+          packet = @interface.read
+        end
+        sleep 0.1
+        thread.kill
+        expect(packet).to be_nil
+      end
+    end
+
+    describe "write" do
+      it "ignores the packet specified" do
+        @interface.instance_variable_set(:@stream, PreStream.new)
+        @interface.add_protocol(IgnorePacketProtocol, ['SYSTEM', 'META'], :WRITE)
+        pkt = System.telemetry.packet("SYSTEM","META")
+        pkt.write("COSMOS_VERSION", "TEST")
+        time = Time.new(2020,1,31,12,15,30.5)
+        pkt.received_time = time
+        $buffer = ''
+        @interface.write(pkt)
+        # Verify the write was ignored
+        expect($buffer).to eql ''
+
+        # Verify reading the interface works
+        $buffer = pkt.buffer
+        packet = @interface.read
+        expect(packet.buffer).to eql $buffer
+      end
+    end
+
+    describe "read/write" do
+      it "ignores the packet specified" do
+        @interface.instance_variable_set(:@stream, PreStream.new)
+        @interface.add_protocol(IgnorePacketProtocol, ['SYSTEM', 'META'], :READ_WRITE)
+        pkt = System.telemetry.packet("SYSTEM","META")
+        pkt.write("COSMOS_VERSION", "TEST")
+        time = Time.new(2020,1,31,12,15,30.5)
+        pkt.received_time = time
+        $buffer = ''
+        @interface.write(pkt)
+        # Verify the write was ignored
+        expect($buffer).to eql ''
+
+        packet = nil
+        # Try to read the interface
+        thread = Thread.new do
+          packet = @interface.read
+        end
+        sleep 1
+        thread.kill
+        expect(packet).to be_nil
+      end
+
+      it "reads and writes unknown packets" do
+        @interface.instance_variable_set(:@stream, PreStream.new)
+        @interface.add_protocol(IgnorePacketProtocol, ['SYSTEM', 'META'], :READ_WRITE)
+        $buffer = ''
+        pkt = Packet.new("TGT","PTK")
+        pkt.append_item("ITEM", 8, :INT)
+        pkt.write("ITEM", 33, :RAW)
+        @interface.write(pkt)
+        # Verify the write went out
+        expect(pkt.buffer).to eql $buffer
+
+        # Verify the read works
+        packet = @interface.read
+        expect(packet.buffer).to eq $buffer
+      end
+    end
+  end
+end
