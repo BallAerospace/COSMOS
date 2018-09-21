ddc1d2600a4399a9f39a045137a806ef65e29ee6
Wildey, Joshua <jwildey@ball.com>
2018-09-21 17:34:28 -0600

    version 0.0.3 published.  include telemetry screen previews!

--- extensions/vscode/src/extension.ts
+++ extensions/vscode/src/extension.ts
@@ -1,5 +1,7 @@
 
-import { ExtensionContext, window } from 'vscode';
+import { commands, ExtensionContext, window, workspace } from 'vscode';
+
+
 
 import {
 	LanguageClient,
@@ -9,13 +11,64 @@ let client: LanguageClient;
 
 export function activate(context: ExtensionContext) {
 
-	// Display a message box to the user
-	window.showInformationMessage('COSMOS Extension!');
+	// create a telemetry screen viewer
+	let screenViewer = new CosmosScreenViewer(context.extensionPath);
+	
+	// Use Console to log diagnostic info
+	console.log('COSMOS Extension!');
+
+	let disposable = commands.registerCommand('cosmos.showGuiPreview', () => {
+        screenViewer.previewScreen();
+    });
+
+    // Add to a list of disposables which are disposed when this extension is deactivated.
+    context.subscriptions.push(screenViewer);
+    context.subscriptions.push(disposable);
 }
 
-export function deactivate(): Thenable<void> {
-	if (!client) {
-		return undefined;
+class CosmosScreenViewer {
+
+	extPath: string;
+
+	constructor(path: string) {
+		this.extPath = path;
 	}
-	return client.stop();
+
+	public previewScreen () {
+
+		// Get the current text editor
+        let editor = window.activeTextEditor;
+        if (!editor) {
+            return;
+		}
+
+		// Get required modules
+		const exec = require("child_process").exec;
+		const path = require('path');
+
+		// Store current working directory
+		let currDir = process.cwd();
+		// Change to directory where current file is because cosmos depends on directories
+		try {
+			process.chdir(path.dirname(editor.document.fileName));
+		}
+		catch (err) {
+		}
+
+		// Build up Ruby Command
+		let rubyCmd = 'ruby ' + this.extPath + '/src/screen_preview.rb ' + editor.document.fileName;
+
+		// Execute Ruby Command
+		exec(rubyCmd, {env: {'PATH': 'C:\\Ruby24-x64\\bin'}}, function(err, stdout, stderr) { });
+
+		// Change back to directory current working directory
+		try {
+			process.chdir(currDir);
+		}
+		catch (err) {
+		}
+	}
+
+	dispose() {
+    }
 }
