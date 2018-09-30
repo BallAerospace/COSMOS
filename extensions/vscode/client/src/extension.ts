859589dd570d770b0aa66490691cb14d50341729
jwildey <jwildey@gmail.com>
2018-09-30 15:51:24 -0600

    client successfully launching server and can debug

--- extensions/vscode/client/src/extension.ts
+++ extensions/vscode/client/src/extension.ts
@@ -14,7 +14,7 @@ export function activate(context: ExtensionContext) {
 
 	// The server is implemented in node
 	let serverModule = context.asAbsolutePath(
-		path.join('server', 'out', 'server.js')
+		path.join('server', 'server.js')
 	);
 
 	// The debug options for the server
