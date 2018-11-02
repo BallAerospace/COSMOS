c034cfd31cca2d4e94ce58775996ae38b3df0c3c
Jason Thomas <jmthomas@ball.com>
2018-11-02 10:36:13 -0600

    Restore sleeper in initialize

--- demo/lib/example_background_task.rb
+++ demo/lib/example_background_task.rb
@@ -18,6 +18,8 @@ module Cosmos
     def initialize
       super()
       @name = 'Example Background Task'
+      # Create Sleeper here in case the task is stopped before call is executed
+      @sleeper = Sleeper.new
     end
 
     def call
