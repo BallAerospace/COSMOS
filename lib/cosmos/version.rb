68e9c7fad431c46eb5bd3a9f4bc9492380a81cbd
= <=>
2019-06-28 17:11:53 -0600

    Release COSMOS 4.4.0

--- lib/cosmos/version.rb
+++ lib/cosmos/version.rb
@@ -1,12 +1,12 @@
 # encoding: ascii-8bit
 
-COSMOS_VERSION = '4.3.0'
+COSMOS_VERSION = '4.4.0'
 module Cosmos
   module Version
     MAJOR = '4'
-    MINOR = '3'
+    MINOR = '4'
     PATCH = '0'
-    BUILD = '3588276d89cb9d934e0e51ad8c6e2426fc5dafb5'
+    BUILD = '30bd354efce074e6684f2fb4f4df8dc12d2f3df5'
   end
-  VERSION = '4.3.0'
+  VERSION = '4.4.0'
 end
