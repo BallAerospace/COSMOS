4ad6443232411f0c46fc100328b38e77355debb0
Melton, Ryan <rmelton@ball.com>
2019-06-28 12:34:45 -0600

    re #1022. Also prevent overlapping items in tables.  Addresses other bug found by Nathan

--- lib/cosmos/tools/table_manager/table_config.rb
+++ lib/cosmos/tools/table_manager/table_config.rb
@@ -172,6 +172,8 @@ module Cosmos
     # duplicated until the specified number of rows are created.
     def finish_packet
       if @current_packet
+        warnings = @current_packet.check_bit_offsets
+        raise "Overlapping items not allowed in tables.\n#{warnings}" if warnings.length > 0
         if @current_packet.type == :TWO_DIMENSIONAL
           items = @current_packet.sorted_items.clone
           (@current_packet.num_rows - 1).times do |row|
