35db7030875f32676b63e8916901dbae50f72e07
Melton, Ryan <rmelton@ball.com>
2020-03-09 18:37:11 +0000

    Release COSMOS 4.4.2

--- lib/cosmos/version.rb
+++ lib/cosmos/version.rb
@@ -1,12 +1,12 @@
 # encoding: ascii-8bit
 
-COSMOS_VERSION = '4.4.1'
+COSMOS_VERSION = '4.4.2'
 module Cosmos
   module Version
     MAJOR = '4'
     MINOR = '4'
-    PATCH = '1'
-    BUILD = 'f8e9c89b2d7fcd0c558806ff6c9dd59d167786e4'
+    PATCH = '2'
+    BUILD = '41a1376a7b11d7bb3498dd6fd0a13c1a9b383946'
   end
-  VERSION = '4.4.1'
+  VERSION = '4.4.2'
 end
