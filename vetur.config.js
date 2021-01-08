4d77004f8c827b4e238c4b270543034855a15511
Thomas, Jason (Software) <jmthomas@ball.com>
2021-01-08 08:55:50 -0700

    Add vetur.config.js jsconfig.json DateTimeChooser

---
+++ vetur.config.js
@@ -0,0 +1,16 @@
+// vetur.config.js
+/** @type {import('vls').VeturConfig} */
+module.exports = {
+  // **optional** default: `{}`
+  // override vscode settings
+  // Notice: It only affects the settings used by Vetur.
+  settings: {
+    "vetur.useWorkspaceDependencies": true,
+    "vetur.experimental.templateInterpolationService": false
+  },
+  // **optional** default: `[{ root: './' }]`
+  // support monorepos
+  projects: [
+    './web/frontend', // shorthand for only root.
+  ]
+}
