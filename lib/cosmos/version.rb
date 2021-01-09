e88c318fdf324842d12979af8ba3cb818b222def
Melton, Ryan <rmelton@ball.com>
2021-01-09 15:04:03 -0700

    udpate version

--- lib/cosmos/version.rb
+++ lib/cosmos/version.rb
@@ -1,29 +1,12 @@
 # encoding: ascii-8bit
 
-# Copyright 2021 Ball Aerospace & Technologies Corp.
-# All Rights Reserved.
-#
-# This program is free software; you can modify and/or redistribute it
-# under the terms of the GNU Affero General Public License
-# as published by the Free Software Foundation; version 3 with
-# attribution addendums as found in the LICENSE.txt
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-#
-# This program may also be used under the terms of a commercial or
-# enterprise edition license of COSMOS if purchased from the
-# copyright holder
-
 COSMOS_VERSION = '5.0.0'
 module Cosmos
   module Version
     MAJOR = '5'
     MINOR = '0'
     PATCH = '0'
-    BUILD = '48928b6fe0b9b110ef3a6c613b7eae5e25643916'
+    BUILD = '4018499331b33e145abf128e13f31eeadd3f3c48'
   end
   VERSION = '5.0.0'
 end
