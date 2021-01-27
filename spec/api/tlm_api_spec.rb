483a8820068cf392af4319d25fe205ee4c413c6c
Thomas, Jason (Software) <jmthomas@ball.com>
2021-01-27 14:07:28 -0700

    Add longer delay to inject_tlm

--- spec/api/tlm_api_spec.rb
+++ spec/api/tlm_api_spec.rb
@@ -311,12 +311,12 @@ module Cosmos
 
       it "injects a packet into the system" do
         @api.inject_tlm("INST","HEALTH_STATUS",{TEMP1: 10, TEMP2: 20}, :CONVERTED, true, true, false)
-        sleep 0.3
+        sleep 0.5
         expect(@api.tlm("INST HEALTH_STATUS TEMP1")).to be_within(0.1).of(10.0)
         expect(@api.tlm("INST HEALTH_STATUS TEMP2")).to be_within(0.1).of(20.0)
 
         @api.inject_tlm("INST","HEALTH_STATUS",{TEMP1: 0, TEMP2: 0}, :RAW, true, true, false)
-        sleep 0.3
+        sleep 0.5
         expect(@api.tlm("INST HEALTH_STATUS TEMP1")).to eql(-100.0)
         expect(@api.tlm("INST HEALTH_STATUS TEMP2")).to eql(-100.0)
       end
