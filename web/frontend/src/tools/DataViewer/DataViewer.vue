878f162d39b7491db4c8fa5e0dca1b595ea51915
Pratt, Ryan <rpratt@ball.com>
2021-02-11 16:59:26 -0700

    Add ability to add and remove new tabs and components to DataViewer

--- web/frontend/src/tools/DataViewer/DataViewer.vue
+++ web/frontend/src/tools/DataViewer/DataViewer.vue
@@ -20,7 +20,7 @@
 <template>
   <div>
     <app-nav app :menus="menus" />
-    <v-card :key="renderKey">
+    <v-card>
       <div class="mb-1">
         <v-alert type="warning" v-model="warning" dismissible>
           {{ warningText }}
@@ -33,9 +33,16 @@
         </v-alert>
       </div>
       <v-tabs ref="tabs" v-model="curTab">
-        <v-tab v-for="(tab, index) in config.tabs" :key="index">
+        <v-tab
+          v-for="(tab, index) in config.tabs"
+          :key="index"
+          @contextmenu="(event) => tabMenu(event, index)"
+        >
           {{ tab.name }}
         </v-tab>
+        <v-btn class="mt-2 ml-2" @click="openTabDialog" icon>
+          <v-icon>mdi-plus-circle</v-icon>
+        </v-btn>
       </v-tabs>
       <v-tabs-items v-model="curTab">
         <v-tab-item v-for="(tab, index) in config.tabs" :key="index" eager>
@@ -44,20 +51,30 @@
             :key="`${index}-${packetIndex}`"
             flat
           >
-            <v-card-title>{{ packet.target }} {{ packet.packet }}</v-card-title>
+            <v-card-title>
+              {{ packet.target }} {{ packet.packet }}
+              <v-spacer />
+              <v-btn @click="() => deleteComponent(index, packetIndex)" icon>
+                <v-icon color="red">mdi-delete</v-icon>
+              </v-btn>
+            </v-card-title>
             <dump-component
-              v-show="receivedPackets[subscriptionKey(packet)]"
-              :ref="`${subscriptionKey(packet)}-display`"
+              v-show="receivedPackets[topicKey(packet)]"
+              :ref="`${topicKey(packet)}-display`"
               :config="packet.config"
-              v-on:config-change="(newConfig) => (packet.config = newConfig)"
+              @config-change="(newConfig) => (packet.config = newConfig)"
             />
-            <v-card-text v-if="!receivedPackets[subscriptionKey(packet)]">
+            <v-card-text v-if="!receivedPackets[topicKey(packet)]">
               No data
             </v-card-text>
           </v-card>
+          <v-btn block @click="() => openComponentDialog(index)">
+            <v-icon>mdi-plus-circle</v-icon>
+          </v-btn>
         </v-tab-item>
       </v-tabs-items>
     </v-card>
+    <!-- Dialogs for opening and saving configs -->
     <OpenConfigDialog
       v-if="openConfig"
       v-model="openConfig"
@@ -70,6 +87,52 @@
       :tool="toolName"
       @success="saveConfiguration($event)"
     />
+    <!-- Dialog for adding a new tab -->
+    <v-dialog v-model="addTabDialog" width="500">
+      <v-card>
+        <v-card-title> Add a tab </v-card-title>
+        <v-card-text>
+          <v-text-field v-model="newTabName" label="Tab name" />
+        </v-card-text>
+        <v-divider></v-divider>
+        <v-card-actions>
+          <v-btn color="primary" text @click="addTab"> Add </v-btn>
+          <v-btn color="primary" text @click="cancelAddTab"> Cancel </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+    <!-- Menu for right clicking on a tab -->
+    <v-menu
+      v-model="showTabMenu"
+      :position-x="tabMenuX"
+      :position-y="tabMenuY"
+      absolute
+      offset-y
+    >
+      <v-list>
+        <v-list-item>
+          <v-list-item-title style="cursor: pointer" @click="deleteTab">
+            Close tab
+          </v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
+    <!-- Dialog for adding a new component to a tab -->
+    <v-dialog v-model="addComponentDialog">
+      <v-card>
+        <v-card-title> Add a packet </v-card-title>
+        <v-card-text>
+          <TargetPacketItemChooser @on-set="packetSelected($event)" />
+        </v-card-text>
+        <v-divider></v-divider>
+        <v-card-actions>
+          <v-btn color="primary" text @click="addComponent"> Add </v-btn>
+          <v-btn color="primary" text @click="cancelAddComponent">
+            Cancel
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </div>
 </template>
 
@@ -79,6 +142,7 @@ import * as ActionCable from 'actioncable'
 import { CosmosApi } from '@/services/cosmos-api'
 import OpenConfigDialog from '@/components/OpenConfigDialog'
 import SaveConfigDialog from '@/components/SaveConfigDialog'
+import TargetPacketItemChooser from '@/components/TargetPacketItemChooser'
 import DumpComponent from './DumpComponent.vue'
 
 export default {
@@ -86,6 +150,7 @@ export default {
     AppNav,
     OpenConfigDialog,
     SaveConfigDialog,
+    TargetPacketItemChooser,
     DumpComponent,
   },
   data() {
@@ -122,7 +187,6 @@ export default {
       error: false,
       errorText: '',
       connectionFailure: false,
-      renderKey: 0,
       config: {
         tabs: [
           {
@@ -145,6 +209,14 @@ export default {
           },
         ],
       },
+      addTabDialog: false,
+      newTabName: '',
+      showTabMenu: false,
+      tabMenuX: 0,
+      tabMenuY: 0,
+      adtiveTab: 0,
+      addComponentDialog: false,
+      newPacket: null,
     }
   },
   watch: {
@@ -193,20 +265,20 @@ export default {
         }
       )
     },
-    addPacketsToSubscription: function () {
+    addPacketsToSubscription: function (packets) {
       this.subscription.perform('add', {
         scope: 'DEFAULT',
-        packets: this.subscriptionPackets,
+        packets: packets || this.allPacketSubscriptionKeys,
         // start_time: 1609532973000000000, // use to hit the file cache
         start_time: Date.now() * 1000000,
         end_time: null,
         mode: 'RAW',
       })
     },
-    removePacketsFromSubscription: function () {
+    removePacketsFromSubscription: function (packets) {
       this.subscription.perform('remove', {
         scope: 'DEFAULT',
-        packets: this.subscriptionPackets,
+        packets: packets || this.allPacketSubscriptionKeys,
       })
     },
     received: function (json_data) {
@@ -223,27 +295,83 @@ export default {
       this.receivedPackets[packetName] = true
       this.receivedPackets = { ...this.receivedPackets } // TODO: why is reactivity broken?
     },
-    subscriptionKey: function (packet) {
+    topicKey: function (packet) {
       return `DEFAULT__TELEMETRY__${packet.target}__${packet.packet}`
     },
-    async openConfiguration(name) {
+    subscriptionKey: function (packet) {
+      return `TLM__${packet.target}__${packet.packet}`
+    },
+    openConfiguration: async function (name) {
       this.removePacketsFromSubscription()
       this.receivedPackets = {}
       let response = await this.api.load_config(this.toolName, name)
       this.config = JSON.parse(response)
-      this.renderKey++ // Trigger re-render
       this.addPacketsToSubscription()
     },
-    saveConfiguration(name) {
+    saveConfiguration: function (name) {
       this.api.save_config(this.toolName, name, JSON.stringify(this.config))
+    },
+    openTabDialog: function () {
+      this.addTabDialog = true
+    },
+    addTab: function () {
+      this.config.tabs.push({
+        name: this.newTabName,
+        packets: [],
+      })
+      this.cancelAddTab()
+    },
+    cancelAddTab: function () {
+      this.addTabDialog = false
+      this.newTabName = ''
+    },
+    tabMenu: function (event, index) {
+      this.adtiveTab = index
+      event.preventDefault()
+      this.showTabMenu = false
+      this.tabMenuX = event.clientX
+      this.tabMenuY = event.clientY
+      this.$nextTick(() => {
+        this.showTabMenu = true
+      })
+    },
+    deleteTab: function () {
+      this.config.tabs.splice(this.adtiveTab, 1)
+    },
+    openComponentDialog: function (index) {
+      this.activeTab = index
+      this.addComponentDialog = true
+    },
+    packetSelected: function (event) {
+      this.newPacket = {
+        target: event.targetName,
+        packet: event.packetName,
+      }
+    },
+    addComponent: function () {
+      const packet = {
+        ...this.newPacket,
+        component: 'DumpComponent',
+        config: {},
+      }
+      this.config.tabs[this.activeTab].packets.push(packet)
+      this.addPacketsToSubscription([this.subscriptionKey(packet)])
+      this.cancelAddComponent()
+    },
+    cancelAddComponent: function () {
+      this.addComponentDialog = false
+      this.newPacket = null
+    },
+    deleteComponent: function (tabIndex, packetIndex) {
+      const packet = this.config.tabs[tabIndex].packets[packetIndex]
+      this.config.tabs[tabIndex].packets.splice(packetIndex, 1)
+      this.removePacketsFromSubscription([this.subscriptionKey(packet)])
     },
   },
   computed: {
-    subscriptionPackets: function () {
+    allPacketSubscriptionKeys: function () {
       return this.config.tabs.flatMap((tab) => {
-        return tab.packets.map(
-          (packet) => 'TLM__' + packet.target + '__' + packet.packet
-        )
+        return tab.packets.map(this.subscriptionKey)
       })
     },
   },
