1eaad68542cd84eb1469f07a5086ef3847cdaea4
Thomas, Jason (Software) <jmthomas@ball.com>
2021-02-11 09:41:29 -0700

    Bump inject wait

--- cosmos/spec/api/tlm_api_spec.rb
+++ cosmos/spec/api/tlm_api_spec.rb
@@ -301,12 +301,12 @@ module Cosmos
 
       it "injects a packet into the system" do
         @api.inject_tlm("INST", "HEALTH_STATUS", {TEMP1: 10, TEMP2: 20}, type: :CONVERTED)
-        sleep 0.1
+        sleep 0.2
         expect(@api.tlm("INST HEALTH_STATUS TEMP1")).to be_within(0.1).of(10.0)
         expect(@api.tlm("INST HEALTH_STATUS TEMP2")).to be_within(0.1).of(20.0)
 
         @api.inject_tlm("INST", "HEALTH_STATUS", {TEMP1: 0, TEMP2: 0}, type: :RAW)
-        sleep 0.1
+        sleep 0.2
         expect(@api.tlm("INST HEALTH_STATUS TEMP1")).to eql(-100.0)
         expect(@api.tlm("INST HEALTH_STATUS TEMP2")).to eql(-100.0)
       end
