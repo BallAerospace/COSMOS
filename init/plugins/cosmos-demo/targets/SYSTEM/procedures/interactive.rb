793a8ab7f8c71ba072e87fbfb54ebbc186e41212
Thomas, Jason (Software) <jmthomas@ball.com>
2021-02-23 11:35:36 -0700

    Remove old false param

--- init/plugins/cosmos-demo/targets/SYSTEM/procedures/interactive.rb
+++ init/plugins/cosmos-demo/targets/SYSTEM/procedures/interactive.rb
@@ -16,11 +16,7 @@ answer = prompt("This is a test", font_size: 30, details: "Some more stuff", inf
 puts "answer:#{answer}"
 answer = message_box('This is a message box', 'one', 'two', 'three', text_color: 'blue', background_color: 'grey', font_size: 20, font_family: 'courier', informative: "Informative stuff", details: "Some more stuff")
 puts "answer:#{answer}"
-answer = message_box('This is a message box', 'one', 'two', 'three', false, background_color: 'grey', details: "Some more stuff")
-puts "answer:#{answer}"
 answer = vertical_message_box('This is a message box', 'one', 'two', 'three', text_color: 'blue', background_color: 'grey', font_size: 20, font_family: 'courier', informative: "Informative stuff", details: "Some more stuff")
-puts "answer:#{answer}"
-answer = vertical_message_box('This is a message box', 'one', 'two', 'three', false, text_color: 'green')
 puts "answer:#{answer}"
 answer = ask("Let me ask you a question", "default")
 puts "answer:#{answer} class:#{answer.class}"
