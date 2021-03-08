63ea231ea708c42cb2cb5c6e4d0fc264aa3b8b21
Thomas, Jason (Software) <jmthomas@ball.com>
2021-03-08 11:15:45 -0700

    frontend-init and npm default port

--- scripts/linux/cosmos_deploy.sh
+++ scripts/linux/cosmos_deploy.sh
@@ -5,7 +5,7 @@ docker tag cosmos-base localhost:5000/cosmos-base:latest
 docker tag cosmos-gems localhost:5000/cosmos-gems:latest
 docker tag cosmos-cmd-tlm-api localhost:5000/cosmos-cmd-tlm-api:latest
 docker tag cosmos-script-runner-api localhost:5000/cosmos-script-runner-api:latest
-docker tag cosmos-frontend localhost:5000/cosmos-frontend:latest
+docker tag cosmos-frontend-init localhost:5000/cosmos-frontend-init:latest
 docker tag cosmos-operator localhost:5000/cosmos-operator:latest
 docker tag cosmos-init localhost:5000/cosmos-init:latest
 
@@ -13,7 +13,7 @@ docker push localhost:5000/cosmos-base:latest
 docker push localhost:5000/cosmos-gems:latest
 docker push localhost:5000/cosmos-cmd-tlm-api:latest
 docker push localhost:5000/cosmos-script-runner-api:latest
-docker push localhost:5000/cosmos-frontend:latest
+docker push localhost:5000/cosmos-frontend-init:latest
 docker push localhost:5000/cosmos-operator:latest
 docker push localhost:5000/cosmos-init:latest
 
@@ -24,7 +24,6 @@ if [[ "$1" == "dev" ]]; then
   docker tag cosmos-grafana localhost:5000/cosmos-grafana:latest
   docker tag cosmos-prometheus localhost:5000/cosmos-prometheus:latest
 
-  docker push localhost:5000/cosmos-aggregator:latest
   docker push localhost:5000/cosmos-elasticsearch:latest
   docker push localhost:5000/cosmos-kibana:latest
   docker push localhost:5000/cosmos-fluentd:latest
