278efdeaf4ca05cf247b846bfdef67bc9507d770
Pratt, Ryan <rpratt@ball.com>
2021-03-16 15:07:28 -0600

    Move classification banner code to own component

--- frontend/cosmos-tool-base/src/App.vue
+++ frontend/cosmos-tool-base/src/App.vue
@@ -18,7 +18,7 @@
 -->
 
 <template>
-  <v-app id="app" :style="classificationStyleVariables">
+  <v-app id="app" :style="classificationStyles">
     <!-- Sizes your content based upon application components -->
     <v-main>
       <!-- Provides the application the proper gutter -->
@@ -34,95 +34,13 @@
 <script>
 import AppFooter from '@/AppFooter'
 import TimeCheck from '@/components/TimeCheck'
-import { CosmosApi } from '@/services/cosmos-api'
+import ClassificationBanners from '@/components/ClassificationBanners'
 
 export default {
   components: {
     AppFooter,
     TimeCheck,
   },
-  data: function () {
-    return {
-      api: null,
-      classification: {
-        text: '',
-        color: 'red',
-        topHeight: 0,
-        bottomHeight: 0,
-      },
-    }
-  },
-  computed: {
-    classificationStyleVariables: function () {
-      // JavaScript can't access CSS psudo-elements (::before and ::after).
-      // This string sets these JS values to CSS variables, accessible to
-      // the style sheet via the style attribute on #app
-      return [
-        `--classification-text:"${this.classification.text}";`,
-        `--classification-color:${this.classification.color};`,
-        `--classification-height-top:${this.classification.topHeight}px;`,
-        `--classification-height-bottom:${this.classification.bottomHeight}px;`,
-      ].join('')
-    },
-  },
-  created: function () {
-    this.api = new CosmosApi()
-    this.loadClassificationBanner()
-  },
-  methods: {
-    loadClassificationBanner: function () {
-      this.api.get_setting('classification_banner').then((response) => {
-        if (response) {
-          this.classification = JSON.parse(response)
-        }
-      }).catch((error) => {
-        console.error('error loading:', error)
-      })
-    },
-  },
+  mixins: [ClassificationBanners],
 }
 </script>
-
-<style>
-/* push things up and down to make room for the classification banners */
-#app,
-#cosmos-nav-drawer {
-  margin-top: var(--classification-height-top);
-}
-#cosmos-app-toolbar {
-  top: var(--classification-height-top);
-}
-#cosmos-nav-drawer .v-navigation-drawer__content {
-  /* this hardcoded 44px will need to be changed if more
-  buttons are added to the nav drawer append section */
-  height: calc(
-    100% - var(--classification-height-top) -
-      var(--classification-height-bottom) - 44px
-  );
-}
-#footer {
-  margin-bottom: var(--classification-height-bottom);
-}
-
-/* make the classification banners */
-#app::before,
-#app::after {
-  z-index: 99;
-  position: fixed;
-  left: 0;
-  right: 0;
-  text-align: center;
-  content: var(--classification-text);
-  background-color: var(--classification-color);
-}
-#app::before {
-  top: 0;
-  font-size: calc(var(--classification-height-top) * 0.7);
-  height: var(--classification-height-top);
-}
-#app::after {
-  bottom: 0;
-  font-size: calc(var(--classification-height-bottom) * 0.7);
-  height: var(--classification-height-bottom);
-}
-</style>
