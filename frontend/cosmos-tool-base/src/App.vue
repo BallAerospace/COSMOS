111449e4824e3a2eba42662fc9818e2d4e4d526e
Pratt, Ryan <rpratt@ball.com>
2021-03-16 15:07:28 -0600

    Add classification banners

--- frontend/cosmos-tool-base/src/App.vue
+++ frontend/cosmos-tool-base/src/App.vue
@@ -18,7 +18,7 @@
 -->
 
 <template>
-  <v-app id="app">
+  <v-app id="app" :style="classificationStyleVariables">
     <!-- Sizes your content based upon application components -->
     <v-main>
       <!-- Provides the application the proper gutter -->
@@ -26,7 +26,7 @@
         <router-view />
       </v-container>
     </v-main>
-    <AppFooter app />
+    <app-footer app />
     <time-check />
   </v-app>
 </template>
@@ -40,7 +40,72 @@ export default {
     AppFooter,
     TimeCheck,
   },
+  data: function () {
+    return {
+      classification: {
+        text: '',
+        backgroundColor: 'red',
+        topHeight: 0,
+        bottomHeight: 0,
+      },
+    }
+  },
+  computed: {
+    classificationStyleVariables: function () {
+      // JavaScript can't access CSS psudo-elements (::before and ::after).
+      // This string sets these JS values to CSS variables, accessible to
+      // the style sheet via the style attribute on #app
+      return [
+        `--classification-text:"${this.classification.text}";`,
+        `--classification-color:${this.classification.backgroundColor};`,
+        `--classification-height-top:${this.classification.topHeight}px;`,
+        `--classification-height-bottom:${this.classification.bottomHeight}px;`,
+      ].join('')
+    },
+  },
 }
 </script>
 
-<style></style>
+<style>
+/* push things up and down to make room for the classification banners */
+#app,
+#cosmos-nav-drawer {
+  margin-top: var(--classification-height-top);
+}
+#cosmos-app-toolbar {
+  top: var(--classification-height-top);
+}
+#cosmos-nav-drawer .v-navigation-drawer__content {
+  /* this hardcoded 44px will need to be changed if more
+  buttons are added to the nav drawer append section */
+  height: calc(
+    100% - var(--classification-height-top) -
+      var(--classification-height-bottom) - 44px
+  );
+}
+#footer {
+  margin-bottom: var(--classification-height-bottom);
+}
+
+/* make the classification banners */
+#app::before,
+#app::after {
+  z-index: 99;
+  position: fixed;
+  left: 0;
+  right: 0;
+  text-align: center;
+  content: var(--classification-text);
+  background-color: var(--classification-color);
+}
+#app::before {
+  top: 0;
+  font-size: calc(var(--classification-height-top) * 0.7);
+  height: var(--classification-height-top);
+}
+#app::after {
+  bottom: 0;
+  font-size: calc(var(--classification-height-bottom) * 0.7);
+  height: var(--classification-height-bottom);
+}
+</style>
