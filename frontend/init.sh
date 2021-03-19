efe3034d240098da3b478f0c42c967e7dd25fc73
Thomas, Jason (Software) <jmthomas@ball.com>
2021-03-19 16:34:21 -0600

    Explictly set DEFAULT scope in init scripts

--- frontend/init.sh
+++ frontend/init.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 set -e
-
-ruby /cosmos/bin/cosmos load /cosmos/plugins/cosmos-tool-base/cosmos-tool-base-5.0.0.*.gem
+ruby /cosmos/bin/cosmos load /cosmos/plugins/cosmos-tool-base/cosmos-tool-base-5.0.0.*.gem DEFAULT
--- init/init.sh
+++ init/init.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 set -e
-ruby /cosmos/bin/cosmos load /cosmos/plugins/cosmos-demo/cosmos-demo-5.0.0.*.gem
+ruby /cosmos/bin/cosmos load /cosmos/plugins/cosmos-demo/cosmos-demo-5.0.0.*.gem DEFAULT
