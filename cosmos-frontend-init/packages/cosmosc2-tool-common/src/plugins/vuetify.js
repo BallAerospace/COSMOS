1635e5db2575eb3cfd56adcd33c2260d72397ee1
Pratt, Ryan <rpratt@ball.com>
2021-04-08 15:58:00 -0600

    Clean up dependencies

--- cosmos-frontend-init/packages/cosmosc2-tool-common/src/plugins/vuetify.js
+++ cosmos-frontend-init/packages/cosmosc2-tool-common/src/plugins/vuetify.js
@@ -19,7 +19,7 @@
 
 import Vue from 'vue'
 import Vuetify from 'vuetify/lib'
-import { AstroIconVuetifyValues } from '@cosmosc2/tool-common/src/components/icons/index.js'
+import { AstroIconVuetifyValues } from '@components/icons/index.js'
 
 Vue.use(Vuetify)
 
