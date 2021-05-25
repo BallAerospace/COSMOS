c6e11a96c05f5a778d4107fd7625145670f0b706
Melton, Ryan <rmelton@ball.com>
2021-05-24 22:33:14 -0600

    build cosmos-traefik

--- scripts/windows/cosmos_build.bat
+++ scripts/windows/cosmos_build.bat
@@ -6,6 +6,7 @@ docker build -f cosmos\Dockerfile -t cosmos-base cosmos || exit /b
 docker build -f cosmos-cmd-tlm-api\Dockerfile -t cosmos-cmd-tlm-api cosmos-cmd-tlm-api || exit /b
 docker build -f cosmos-script-runner-api\Dockerfile -t cosmos-script-runner-api cosmos-script-runner-api || exit /b
 docker build -f cosmos-frontend-init\Dockerfile -t cosmos-frontend-init cosmos-frontend-init || exit /b
+docker build -f traefik\Dockerfile -t cosmos-traefik traefik || exit /b
 docker build -f cosmos-operator\Dockerfile -t cosmos-operator cosmos-operator || exit /b
 docker build -f cosmos-init\Dockerfile -t cosmos-init cosmos-init || exit /b
 
