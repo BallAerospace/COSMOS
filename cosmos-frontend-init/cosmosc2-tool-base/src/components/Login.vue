a0967683756fbcf9386e3f7f38e255205eb64de8
Pratt, Ryan <rpratt@ball.com>
2021-05-27 16:44:17 -0600

    Add password frontend

--- cosmos-frontend-init/cosmosc2-tool-base/src/components/Login.vue
+++ cosmos-frontend-init/cosmosc2-tool-base/src/components/Login.vue
@@ -18,27 +18,157 @@
 -->
 
 <template>
-  <div>
-    <top-bar :title="title" />
-
-    <v-card>
-      <v-card-title> Login </v-card-title>
-    </v-card>
-  </div>
+  <v-card>
+    <v-card-title> Login </v-card-title>
+    <v-card-subtitle>
+      Enter the password to begin using COSMOS
+    </v-card-subtitle>
+    <v-card-text>
+      <v-text-field
+        v-model="password"
+        type="password"
+        :label="`${!isSet || reset ? 'New ' : ''}Password`"
+      />
+      <v-text-field
+        v-if="reset"
+        v-model="token"
+        type="password"
+        label="Reset Token"
+      />
+      <v-btn
+        v-if="reset"
+        @click="resetPassword"
+        large
+        color="warn"
+        :disabled="!password || !token"
+      >
+        Reset
+      </v-btn>
+      <v-btn
+        v-else-if="!isSet"
+        @click="setPassword"
+        large
+        color="success"
+        :disabled="!password"
+      >
+        Set
+      </v-btn>
+      <template v-else>
+        <v-btn
+          @click="verifyPassword"
+          large
+          color="success"
+          :disabled="!password"
+        >
+          Login
+        </v-btn>
+        <!-- <v-btn text small @click="showReset"> Reset Password </v-btn> -->
+      </template>
+    </v-card-text>
+    <v-alert :type="alertType" v-model="showAlert" dismissible>
+      {{ alert }}
+    </v-alert>
+  </v-card>
 </template>
 
 <script>
-import TopBar from '../../../packages/cosmosc2-tool-common/src/components/TopBar'
+import Api from '../../../packages/cosmosc2-tool-common/src/services/api'
 
 export default {
-  components: {
-    TopBar,
-  },
   data() {
     return {
-      title: 'COSMOS Login',
+      isSet: true,
+      password: '',
+      token: '',
+      reset: false,
+      alert: '',
+      alertType: 'success',
+      showAlert: false,
     }
   },
-  methods: {},
+  computed: {
+    options: function () {
+      return {
+        noAuth: true,
+        noScope: true, // lol
+      }
+    },
+  },
+  created: function () {
+    Api.get('/cosmos-api/password/exists', null, this.options).then(
+      (response) => {
+        this.isSet = !!response.data.result
+      }
+    )
+  },
+  methods: {
+    showReset: function () {
+      this.reset = true
+    },
+    login: function () {
+      localStorage.token = this.password
+      // TODO: redirect back
+    },
+    verifyPassword: function () {
+      this.showAlert = false
+      Api.post(
+        '/cosmos-api/password/verify',
+        {
+          password: this.password,
+        },
+        null,
+        this.options
+      )
+        .then((response) => {
+          if (response.data.result) {
+            this.login()
+          } else {
+            this.alert = 'Incorrect password'
+            this.alertType = 'warning'
+            this.showAlert = true
+          }
+        })
+        .catch((error) => {
+          this.alert = error
+          this.alertType = 'error'
+          this.showAlert = true
+        })
+    },
+    setPassword: function () {
+      this.showAlert = false
+      Api.post(
+        '/cosmos-api/password/set',
+        {
+          password: this.password,
+        },
+        null,
+        this.options
+      )
+        .then(this.login())
+        .catch((error) => {
+          this.alert = error
+          this.alertType = 'error'
+          this.showAlert = true
+        })
+    },
+    resetPassword: function () {
+      this.showAlert = false
+      Api.post(
+        '/cosmos-api/password/reset',
+        {
+          password: this.password,
+          recovery_token: this.token,
+        },
+        null,
+        this.options
+      )
+        .then(this.login())
+        .catch((error) => {
+          this.alert = error
+          this.alertType = 'error'
+          this.showAlert = true
+        })
+    },
+  },
 }
 </script>
