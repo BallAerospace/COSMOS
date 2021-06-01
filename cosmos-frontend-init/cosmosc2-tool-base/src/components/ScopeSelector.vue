9a69ecdc5ea8ef65c738dfc4242ed0ebb2389d01
Pratt, Ryan <rpratt@ball.com>
2021-06-01 16:27:37 -0600

    Add ScopeSelector

---
+++ cosmos-frontend-init/cosmosc2-tool-base/src/components/ScopeSelector.vue
@@ -0,0 +1,46 @@
+<!--
+# Copyright 2021 Ball Aerospace & Technologies Corp.
+# All Rights Reserved.
+#
+# This program is free software; you can modify and/or redistribute it
+# under the terms of the GNU Affero General Public License
+# as published by the Free Software Foundation; version 3 with
+# attribution addendums as found in the LICENSE.txt
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# This program may also be used under the terms of a commercial or
+# enterprise edition license of COSMOS if purchased from the
+# copyright holder
+-->
+
+<template>
+  <v-select v-model="scope" :items="scopes" label="Scope" dense hide-details />
+</template>
+
+<script>
+import Api from '../../../packages/cosmosc2-tool-common/src/services/api'
+
+export default {
+  data: function () {
+    return {
+      scopes: ['DEFAULT'],
+      scope: localStorage.scope,
+    }
+  },
+  watch: {
+    scope: function (val) {
+      localStorage.scope = val
+    },
+  },
+  created: function () {
+    Api.get('/cosmos-api/scopes').then((response) => {
+      this.scopes = response.data
+      if (!this.scope && this.scopes.length) this.scope = this.scopes[0]
+    })
+  },
+}
+</script>
