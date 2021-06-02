62becefde6f1f06b995899bdda73f52edd5380da
van Andel, Gerhard <gvanande@ball.com>
2021-06-02 15:05:22 -0600

    update for working clock in footer

--- cosmos-frontend-init/cosmosc2-tool-base/src/components/ClockFooter.vue
+++ cosmos-frontend-init/cosmosc2-tool-base/src/components/ClockFooter.vue
@@ -19,15 +19,25 @@
 
 <template>
   <div>
-    <v-tooltip top v-if="localTime !== utcTime">
-      <template>
-        <div v-show="localTime" v-text="localTime" />
+    <v-tooltip top v-if="validOffset">
+      <template v-slot:activator="{ on, attrs }">
+        <span
+          v-bind="attrs"
+          v-on="on"
+          v-text="localTime"
+          style="margin-left: 1em"
+        />
       </template>
       <span> Local Time </span>
     </v-tooltip>
     <v-tooltip top>
-      <template>
-        <div v-show="utcTime" v-text="utcTime" />
+      <template v-slot:activator="{ on, attrs }">
+        <span
+          v-bind="attrs"
+          v-on="on"
+          v-text="utcTime"
+          style="margin-left: 1em"
+        />
       </template>
       <span> UTC Time </span>
     </v-tooltip>
@@ -36,36 +46,48 @@
 
 <script>
 export default {
+  data: function () {
+    return {
+      date: new Date(),
+      intervalCount: 0,
+    }
+  },
+  created: function () {
+    setInterval(
+      () => {
+        this.intervalCount++
+        this.date = new Date()
+      },
+      this.intervalCount < 10 ? 100 : 1000 // get the seconds to about 100ms accuracy
+    )
+  },
   computed: {
+    validOffset: function () {
+      return this.date.getTimezoneOffset() !== 0
+    },
     localTime: function () {
-      // convert the date object to
-      const date = new Date()
-      function pad(num) {
-        var norm = Math.floor(Math.abs(num))
-        return (norm < 10 ? '0' : '') + norm
-      }
-      const year = date.getFullYear()
-      const month = pad(date.getMonth() + 1)
-      const day = pad(date.getDate())
-      const hour = pad(date.getHours())
-      const minute = pad(date.getMinutes())
-      const second = pad(date.getSeconds())
+      const year = this.date.getFullYear()
+      const month = this.pad(this.date.getMonth() + 1)
+      const day = this.pad(this.date.getDate())
+      const hour = this.pad(this.date.getHours())
+      const minute = this.pad(this.date.getMinutes())
+      const second = this.pad(this.date.getSeconds())
       return `${year}-${month}-${day}T${hour}:${minute}:${second}`
     },
     utcTime: function () {
-      // convert the date object to
-      const date = new Date()
-      function pad(num) {
-        var norm = Math.floor(Math.abs(num))
-        return (norm < 10 ? '0' : '') + norm
-      }
-      const year = date.getFullYear()
-      const month = pad(date.getMonth() + 1)
-      const day = pad(date.getDate())
-      const hour = pad(date.getHours())
-      const minute = pad(date.getMinutes())
-      const second = pad(date.getSeconds())
+      const year = this.date.getUTCFullYear()
+      const month = this.pad(this.date.getUTCMonth() + 1)
+      const day = this.pad(this.date.getUTCDate())
+      const hour = this.pad(this.date.getUTCHours())
+      const minute = this.pad(this.date.getUTCMinutes())
+      const second = this.pad(this.date.getUTCSeconds())
       return `${year}-${month}-${day}T${hour}:${minute}:${second}Z`
+    },
+  },
+  methods: {
+    pad: function (num) {
+      var norm = Math.floor(Math.abs(num))
+      return (norm < 10 ? '0' : '') + norm
     },
   },
 }
