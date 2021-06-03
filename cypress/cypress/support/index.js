9515fbd7edf76f57c24e9c067bb7457aae640c88
Pratt, Ryan <rpratt@ball.com>
2021-06-03 17:11:11 -0600

    Fix cypress tests

--- cypress/cypress/support/index.js
+++ cypress/cypress/support/index.js
@@ -42,3 +42,7 @@ Cypress.on('window:before:load', (win) => {
   cy.spy(win.console, 'log').as('consoleLog')
   cy.spy(win.console, 'error').as('consoleError')
 })
+
+Cypress.on('window:load', (win) => {
+  win.localStorage.token = 'password'
+})
