477ef5f642e75391a24360dce38a6fe1d1e53103
Pratt, Ryan <rpratt@ball.com>
2021-06-04 17:15:49 -0600

    Rework clock

--- cosmos-frontend-init/cosmosc2-tool-base/src/components/ClockFooter.vue
+++ cosmos-frontend-init/cosmosc2-tool-base/src/components/ClockFooter.vue
@@ -19,72 +19,162 @@
 
 <template>
   <div>
-    <v-tooltip top v-if="validOffset">
-      <template v-slot:activator="{ on, attrs }">
-        <span v-bind="attrs" v-on="on" v-text="localTime" class="clock-text" />
-      </template>
-      <span> Local Time </span>
-    </v-tooltip>
-    <v-tooltip top>
-      <template v-slot:activator="{ on, attrs }">
-        <span v-bind="attrs" v-on="on" v-text="utcTime" class="clock-text" />
-      </template>
-      <span> UTC Time </span>
-    </v-tooltip>
+    <span
+      v-bind="attrs"
+      v-on="on"
+      style="cursor: context-menu"
+      class="font-weight-bold"
+      @contextmenu="openMenu"
+    >
+      {{ (displayLocal ? localDate : utcDate) | date(formatString) }}
+      ({{ displayLocal ? 'local' : 'UTC' }})
+    </span>
+    <v-menu
+      v-model="showMenu"
+      :position-x="menuX"
+      :position-y="menuY"
+      absolute
+      offset-y
+    >
+      <v-list>
+        <v-list-item>
+          <v-list-item-title
+            style="cursor: pointer"
+            @click="toggleDisplayLocal"
+          >
+            Display {{ displayLocal ? 'UTC' : 'local time' }}
+          </v-list-item-title>
+        </v-list-item>
+        <v-divider />
+        <v-list-item>
+          <v-list-item-title style="cursor: pointer" @click="toggleDisplay24h">
+            Display {{ display24h ? 12 : 24 }} hour clock
+          </v-list-item-title>
+        </v-list-item>
+        <v-divider />
+        <v-list-item v-if="dateMode !== 'monthFirst'">
+          <v-list-item-title style="cursor: pointer" @click="setMonthFirst">
+            Display mm/dd/yyyy
+          </v-list-item-title>
+        </v-list-item>
+        <v-list-item v-if="dateMode !== 'dayFirst'">
+          <v-list-item-title style="cursor: pointer" @click="setDayFirst">
+            Display dd/mm/yyyy
+          </v-list-item-title>
+        </v-list-item>
+        <v-list-item v-if="dateMode !== 'number'">
+          <v-list-item-title style="cursor: pointer" @click="setNumber">
+            Display day of year
+          </v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
   </div>
 </template>
 
 <script>
+import { format } from 'date-fns'
+
 export default {
   data: function () {
     return {
-      date: new Date(),
+      localDate: new Date(),
+      displayLocal: localStorage.clock_zone === 'local',
+      display24h: !localStorage.clozk_12h,
+      dateMode: localStorage.clock_dateMode || 'monthFirst',
       intervalCount: 0,
+      showMenu: false,
+      menuX: 0,
+      menuY: 0,
     }
+  },
+  computed: {
+    utcDate: function () {
+      return new Date(
+        this.localDate.getTime() + this.localDate.getTimezoneOffset() * 60000
+      )
+    },
+    formatString: function () {
+      let dateFormat
+      switch (this.dateMode) {
+        case 'monthFirst':
+          dateFormat = 'LL/dd/yyy'
+          break
+        case 'dayFirst':
+          dateFormat = 'dd/LL/yyy'
+          break
+        case 'number':
+          dateFormat = 'D'
+          break
+      }
+
+      let timeFormat
+      if (this.display24h) {
+        timeFormat = 'HH:mm:ss'
+      } else {
+        timeFormat = 'hh:mm:ss a'
+      }
+
+      return `${dateFormat} ${timeFormat}`
+    },
+  },
+  watch: {
+    displayLocal: function (val) {
+      localStorage.clock_zone = val ? 'local' : 'utc'
+    },
+    display24h: function (val) {
+      // store opposite because it makes default value (true) easier
+      if (val) {
+        delete localStorage.clock_12h
+      } else {
+        localStorage.clock_12h = true
+      }
+    },
+    dateMode: function (val) {
+      if (val.match(/^monthFirst$|^dayFirst$|^number$/)) {
+        localStorage.clock_dateMode = val
+      }
+    },
   },
   created: function () {
     setInterval(
       () => {
         this.intervalCount++
-        this.date = new Date()
+        this.localDate = new Date()
       },
       this.intervalCount < 10 ? 100 : 1000 // get the seconds to about 100ms accuracy
     )
   },
-  computed: {
-    validOffset: function () {
-      return this.date.getTimezoneOffset() !== 0
+  methods: {
+    openMenu: function (event) {
+      event.preventDefault()
+      this.showMenu = false
+      this.menuX = event.clientX
+      this.menuY = event.clientY
+      this.$nextTick(() => {
+        this.showMenu = true
+      })
     },
-    localTime: function () {
-      const year = this.date.getFullYear()
-      const month = this.pad(this.date.getMonth() + 1)
-      const day = this.pad(this.date.getDate())
-      const hour = this.pad(this.date.getHours())
-      const minute = this.pad(this.date.getMinutes())
-      const second = this.pad(this.date.getSeconds())
-      return `${year}-${month}-${day}T${hour}:${minute}:${second}`
+    toggleDisplayLocal: function () {
+      this.displayLocal = !this.displayLocal
     },
-    utcTime: function () {
-      const year = this.date.getUTCFullYear()
-      const month = this.pad(this.date.getUTCMonth() + 1)
-      const day = this.pad(this.date.getUTCDate())
-      const hour = this.pad(this.date.getUTCHours())
-      const minute = this.pad(this.date.getUTCMinutes())
-      const second = this.pad(this.date.getUTCSeconds())
-      return `${year}-${month}-${day}T${hour}:${minute}:${second}Z`
+    toggleDisplay24h: function () {
+      this.display24h = !this.display24h
+    },
+    setMonthFirst: function () {
+      this.dateMode = 'monthFirst'
+    },
+    setDayFirst: function () {
+      this.dateMode = 'dayFirst'
+    },
+    setNumber: function () {
+      this.dateMode = 'number'
     },
   },
-  methods: {
-    pad: function (num) {
-      var norm = Math.floor(Math.abs(num))
-      return (norm < 10 ? '0' : '') + norm
+  filters: {
+    date: function (val, formatString) {
+      return format(val, formatString, { useAdditionalDayOfYearTokens: true })
     },
   },
 }
 </script>
-
-<style scoped>
-.clock-text {
-  margin-left: 1em;
-}
-</style>
