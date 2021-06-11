26ba9d4d1fc9560b35ccb9b24290d9f98f068298
Thomas, Jason (Software) <jmthomas@ball.com>
2021-06-11 16:26:01 -0600

    Update batch and other comments

--- cosmos-control.bat
+++ cosmos-control.bat
@@ -1,7 +1,17 @@
 @echo off
+setlocal ENABLEDELAYEDEXPANSION
 
-if ("%1"=="") (
+if "%1" == "" (
   GOTO usage
+)
+if "%1" == "cosmos" (
+  set params=%*
+  call set params=%%params:*%1=%%
+  REM Start (and remove when done --rm) the cosmos-base container with the current working directory
+  REM mapped as volume (-v) /cosmos/plugins and container working directory (-w) also set to /cosmos/plugins
+  REM and run the command "ruby /cosmos/bin/cosmos" with all parameters ignoring the first
+  docker run --rm -v %cd%:/cosmos/plugins -w /cosmos/plugins cosmos-base ruby /cosmos/bin/cosmos !params!
+  GOTO :EOF
 )
 if "%1" == "restart" (
   GOTO restart
@@ -67,6 +77,7 @@ GOTO :EOF
 
 :usage
   @echo Usage: %0 [start, stop, cleanup, build, run, deploy] 1>&2
+  @echo *  cosmos: run a cosmos command ('cosmos help' for more info) 1>&2
   @echo *  start: run the docker containers for cosmos 1>&2
   @echo *  stop: stop the running docker containers for cosmos 1>&2
   @echo *  restart: stop and run the minimal docker containers for cosmos 1>&2
