8844cb5542b6ef8269d4698d379adf98bb3d9f92
Mircea Zisu <62066453+MirceaZisu@users.noreply.github.com>
2021-07-07 13:59:11 +0100

    Update cosmos/spec/interfaces/simulated_target_interface_spec.rb
    Co-authored-by: Stanislav Pankevich <s.pankevich@gmail.com>

--- cosmos/spec/interfaces/simulated_target_interface_spec.rb
+++ cosmos/spec/interfaces/simulated_target_interface_spec.rb
@@ -70,7 +70,7 @@ end
     end
 
     describe "connected?" do
-      it "initiallized as false" do
+      it "is initialized as false" do
         sti = SimulatedTargetInterface.new("test_inst.rb")
         sti.target_names = ['SYSTEM']
         expect(sti.connected?).to be false
