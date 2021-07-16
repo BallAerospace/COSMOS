774f2af1548840c2f8476cdee53735756e4154ed
Pratt, Ryan <rpratt@ball.com>
2021-07-16 10:30:23 -0600

    Add test cosmos-control scripts

---
+++ scripts/linux/cosmos_test.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env sh
+
+usage() {
+  echo "Usage: $1 [rspec, cypress]" >&2
+  echo "*  rspec: run tests against Ruby code" >&2
+  echo "*  cypress: run end-to-end tests" >&2
+  exit 1
+}
+
+if [[ "$#" -eq 0 ]]; then
+  usage $0
+fi
+
+case $1 in
+rspec)
+  cd cosmos
+  rspec
+  cd -
+  ;;
+hash)
+  docker-compose -f compose.yaml up -d
+  cd cypress
+  yarn
+  yarn run fixlinux
+  yarn run cypress run
+  cd -
+  docker-compose -f compose.yaml down -v
+  ;;
+*)
+  usage $0
+  ;;
+esac
