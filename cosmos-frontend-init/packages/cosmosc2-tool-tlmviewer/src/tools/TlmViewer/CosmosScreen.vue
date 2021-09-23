257abf8e227d34d6aa511fa494bf61ba1c9b1586
van Andel, Gerhard <gvanande@ball.com>
2021-09-23 15:34:55 -0600

    load, download, and edit screens

--- cosmos-frontend-init/packages/cosmosc2-tool-tlmviewer/src/tools/TlmViewer/CosmosScreen.vue
+++ cosmos-frontend-init/packages/cosmosc2-tool-tlmviewer/src/tools/TlmViewer/CosmosScreen.vue
@@ -20,14 +20,43 @@
 <template>
   <div>
     <v-card>
-      <v-system-bar>
+      <v-system-bar height="40">
         <v-icon v-if="errors.length !== 0">mdi-alert</v-icon>
         <v-spacer />
         <span>{{ target }} {{ screen }}</span>
         <v-spacer />
-        <v-icon @click="openEdit">mdi-pencil</v-icon>
-        <v-icon @click="minMaxTransition">mdi-window-minimize</v-icon>
-        <v-icon @click="$emit('close-screen')">mdi-close-box</v-icon>
+        <v-tooltip top>
+          <template v-slot:activator="{ on, attrs }">
+            <div v-on="on" v-bind="attrs">
+              <v-btn icon data-test="editScreenIcon" @click="openEdit">
+                <v-icon> mdi-pencil </v-icon>
+              </v-btn>
+            </div>
+          </template>
+          <span> Edit Screen </span>
+        </v-tooltip>
+        <v-tooltip top>
+          <template v-slot:activator="{ on, attrs }">
+            <div v-on="on" v-bind="attrs">
+              <v-btn icon data-test="minimizeScreenIcon" @click="minMaxTransition">
+                <v-icon v-show="expand"> mdi-window-minimize </v-icon>
+                <v-icon v-show="!expand"> mdi-window-maximize </v-icon>
+              </v-btn>
+            </div>
+          </template>
+          <span v-show="expand"> Minimize Screen </span>
+          <span v-show="!expand"> Minimize Screen </span>
+        </v-tooltip>
+        <v-tooltip top>
+          <template v-slot:activator="{ on, attrs }">
+            <div v-on="on" v-bind="attrs">
+              <v-btn icon data-test="downloadScreenIcon" @click="$emit('close-screen')">
+                <v-icon> mdi-close-box </v-icon>
+              </v-btn>
+            </div>
+          </template>
+          <span> Close Screen </span>
+        </v-tooltip>
       </v-system-bar>
       <v-expand-transition>
         <div class="pa-1" ref="screen" v-show="expand">
@@ -38,21 +67,102 @@
         </div>
       </v-expand-transition>
     </v-card>
-    <v-dialog v-model="editDialog" width="800">
+    <v-dialog v-model="editDialog" width="600">
       <v-card>
-        <v-card-title>
-          <span class="text-h5">Edit Screen</span>
-        </v-card-title>
-        <v-alert type="error" v-model="showSaveAlert" dismissible>
-          {{ saveAlert }}
-        </v-alert>
+        <v-toolbar>
+          <v-toolbar-title>Edit Screen: {{ target }} {{ screen }}</v-toolbar-title>
+          <v-spacer />
+          <div v-show="inputType === 'file'">
+            <v-progress-circular
+              class="mx-2"
+              :indeterminate="readingFile"
+              color="primary"
+            />
+          </div>
+          <div class="mx-2">
+            <v-tooltip top>
+              <template v-slot:activator="{ on, attrs }">
+                <div v-on="on" v-bind="attrs">
+                  <v-btn icon data-test="downloadScreenIcon" @click="downloadScreen">
+                    <v-icon> mdi-download </v-icon>
+                  </v-btn>
+                </div>
+              </template>
+              <span> Download Screen </span>
+            </v-tooltip>
+          </div>
+          <v-menu bottom right>
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn data-test="czml-change-type" outlined v-bind="attrs" v-on="on">
+                <span v-text="inputTypeToLabel[inputType]" />
+                <v-icon right> mdi-menu-down </v-icon>
+              </v-btn>
+            </template>
+            <v-list>
+              <v-list-item @click="inputType = 'txt'" data-test="typeTxt">
+                <v-list-item-title v-text="inputTypeToLabel['txt']" />
+              </v-list-item>
+              <v-list-item @click="inputType = 'file'" data-test="typeFile">
+                <v-list-item-title v-text="inputTypeToLabel['file']" />
+              </v-list-item>
+            </v-list>
+          </v-menu>
+        </v-toolbar>
         <v-card-text>
-          <v-textarea auto-grow v-model="currentDefinition"></v-textarea>
+          <div v-show="inputType === 'file'">
+            <v-row class="mt-3"> Upload a the screen file. </v-row>
+            <v-row>
+              <v-file-input
+                v-model="file"
+                truncate-length="15"
+                accept=".txt"
+              />
+            </v-row>
+          </div>
+          <div v-show="inputType !== 'file'">
+            <v-row>
+              <v-textarea
+                v-model="currentDefinition"
+                rows="12"
+                :rules="[rules.required]" 
+                data-test="screenTextInput"
+              />
+            </v-row>
+          </div>
+          <v-row class="my-3">
+            <span class="red--text" v-show="error" v-text="error" />
+          </v-row>
+          <v-row>
+            <div v-show="inputType !== 'file'">
+              <v-btn
+                color="success"
+                @click="saveEdit"
+                :disabled="!!error"
+                data-test="editScreenSubmitBtn"
+              >
+                Save
+              </v-btn>
+            </div>
+            <div v-show="inputType === 'file'">
+              <v-btn
+                color="success"
+                @click="loadFile"
+                :disabled="!!error"
+                data-test="editScreenLoadBtn"
+              >
+                Load
+              </v-btn>
+            </div>
+            <v-spacer />
+            <v-btn
+              color="primary"
+              @click="cancelEdit"
+              data-test="editScreenCancelBtn"
+            >
+              Cancel
+            </v-btn>
+          </v-row>
         </v-card-text>
-        <v-card-actions>
-          <v-btn color="primary" @click="saveEdit">Save</v-btn>
-          <v-btn color="primary" @click="cancelEdit">Cancel</v-btn>
-        </v-card-actions>
       </v-card>
     </v-dialog>
   </div>
@@ -118,7 +228,17 @@ export default {
   },
   data() {
     return {
+      rules: {
+        required: (value) => !!value || 'Required',
+      },
       api: null,
+      readingFile: false,
+      file: null,
+      inputType: 'txt',
+      inputTypeToLabel: {
+        txt: 'TXT',
+        file: 'FILE',
+      },
       currentDefinition: this.definition,
       backup: '',
       editDialog: false,
@@ -137,9 +257,19 @@ export default {
       force_substitute: false,
       pollingPeriod: 1,
       showSaveAlert: false,
-      saveAlert: '',
       errors: [],
     }
+  },
+  computed: {
+    error: function () {
+      if (this.editDialog && this.errors.length > 0) {
+        return this.errors.toString()
+      }
+      if (this.currentDefinition === '' && !this.file) {
+        return 'input can not be blank.'
+      }
+      return null
+    },
   },
   // Called when an error from any descendent component is captured
   // We need this because an error can occur from any of the children
@@ -163,16 +293,6 @@ export default {
       this.update()
     }, refreshInterval)
   },
-  updated() {
-    this.$nextTick(function () {
-      // This ensures that we only display the errors after they have
-      // been parsed by the errorCaptured handler
-      if (this.editDialog && this.errors.length > 0) {
-        this.saveAlert = this.errors.toString()
-        this.showSaveAlert = true
-      }
-    })
-  },
   destroyed() {
     if (this.updater != null) {
       clearInterval(this.updater)
@@ -180,7 +300,7 @@ export default {
     }
   },
   methods: {
-    parseDefinition() {
+    parseDefinition: function () {
       // Each time we start over and parse the screen definition
       this.showSaveAlert = false
       this.errors = []
@@ -225,10 +345,6 @@ export default {
                 ]
                 break
               case 'SETTING':
-                this.currentLayout.widgets[
-                  this.currentLayout.widgets.length - 1
-                ].settings.push(parameters)
-                break
               case 'SUBSETTING':
                 // Just push it onto the settings array and the widget will figure it out
                 this.currentLayout.widgets[
@@ -242,7 +358,7 @@ export default {
                 this.globalSubsettings.push(parameters)
                 break
               default:
-                this.process_widget(keyword, parameters)
+                this.processWidget(keyword, parameters)
                 break
             } // switch keyword
           } // if keyword
@@ -252,14 +368,14 @@ export default {
     },
     // Called by button scripts to get named widgets
     // Underscores used to match COSMOS API rather than Javascript convention
-    get_named_widget(name) {
+    get_named_widget: function (name) {
       return this.namedWidgets[name]
     },
     // Called by named widgets to register with the screen
-    setNamedWidget(name, widget) {
+    setNamedWidget: function (name, widget) {
       this.namedWidgets[name] = widget
     },
-    update() {
+    update: function () {
       if (this.$store.state.tlmViewerItems.length !== 0) {
         this.api
           .get_tlm_values(this.$store.state.tlmViewerItems)
@@ -268,42 +384,65 @@ export default {
           })
       }
     },
-    openEdit() {
+    openEdit: function () {
       // Make a copy in case they edit and cancel
       this.backup = this.currentDefinition.repeat(1)
       this.editDialog = true
     },
-    cancelEdit() {
+    cancelEdit: function () {
+      this.file = null
       this.editDialog = false
       // Restore the backup since we cancelled
       this.currentDefinition = this.backup
       this.parseDefinition()
     },
-    saveEdit() {
+    loadFile: function () {
+      const fileReader = new FileReader()
+      fileReader.readAsText(this.file)
+      this.readingFile = true
+      const that = this
+      fileReader.onload = function () {
+        that.readingFile = false
+        that.currentDefinition = fileReader.result
+        that.inputType = 'txt'
+        that.file = null
+      }
+    },
+    saveEdit: function () {
       this.parseDefinition()
       // After parsing wait and see if there are errors before saving
       this.$nextTick(function () {
-        if (this.errors.length === 0) {
-          Api.post('/cosmos-api/screen/', {
-            data: {
-              scope: localStorage.scope,
-              target: this.target,
-              screen: this.screen,
-              text: this.currentDefinition,
-            },
-          }).catch((error) => {
-            this.saveAlert = error
-            this.showSaveAlert = true
-          })
-          this.editDialog = false
-        }
+        Api.post('/cosmos-api/screen/', {
+          data: {
+            scope: localStorage.scope,
+            target: this.target,
+            screen: this.screen,
+            text: this.currentDefinition,
+          },
+        }).catch((error) => {
+          this.saveAlert = error
+          this.showSaveAlert = true
+        })
+        this.editDialog = false
       })
     },
-    minMaxTransition() {
+    downloadScreen: function() {
+      const blob = new Blob([this.currentDefinition], {
+        type: 'text/plain',
+      })
+      // Make a link and then 'click' on it to start the download
+      const link = document.createElement('a')
+      link.href = URL.createObjectURL(blob)
+      link.setAttribute(
+        'download', `${this.target}_${this.screen}.txt`
+      )
+      link.click()
+    },
+    minMaxTransition: function () {
       this.expand = !this.expand
       this.$emit('min-max-screen')
     },
-    process_widget(keyword, parameters) {
+    processWidget: function (keyword, parameters) {
       var widgetName = null
       if (keyword === 'NAMED_WIDGET') {
         this.configParser.verify_num_parameters(
@@ -358,7 +497,7 @@ export default {
         })
       }
     },
-    applyGlobalSettings(widgets) {
+    applyGlobalSettings: function (widgets) {
       this.globalSettings.forEach((setting) => {
         widgets.forEach((widget) => {
           // widget.type is already the full camelcase widget name like LabelWidget
