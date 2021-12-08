015f9694c081ad03d6a6fe944f51c231875d94df
van Andel, Gerhard <gvanande@ball.com>
2021-12-08 15:06:14 -0700

    update to add simple example in docker

--- examples/external_script.rb
+++ examples/external_script.rb
@@ -1,13 +1,36 @@
-require 'cosmos'
+# encoding: ascii-8bit
 
+# Copyright 2021 Ball Aerospace & Technologies Corp.
+# All Rights Reserved.
+#
+# This program is free software; you can modify and/or redistribute it
+# under the terms of the GNU Affero General Public License
+# as published by the Free Software Foundation; version 3 with
+# attribution addendums as found in the LICENSE.txt
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# This program may also be used under the terms of a commercial or
+# enterprise edition license of COSMOS if purchased from the
+# copyright holder
+
+require 'cosmos'
 require 'cosmos/script'
 
-
-
-ENV['COSMOS_PASSWORD'] = 'cosmos'
+ENV['COSMOS_API_SCHEMA'] = 'http'
+ENV['COSMOS_API_HOSTNAME'] = 'host.docker.internal'
+ENV['COSMOS_API_PORT'] = '2900'
+ENV['COSMOS_API_PASSWORD'] = 'cosmos'
 
 ENV['COSMOS_NO_STORE'] = '1'
 
+puts get_target_list()
 
+puts get_all_target_info()
 
-cmd("INST ABORT")
+puts tlm('INST ADCS POSX')
+
+puts cmd("INST ABORT")
