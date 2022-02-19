0331799758b0632437c619624ed4fbfe1d1f2531
Thomas, Jason (Software) <jmthomas@ball.com>
2022-02-19 08:22:50 -0700

    Upgrade vue cli v5

--- cosmos-init/plugins/packages/cosmosc2-tool-cmdsender/vue.config.js
+++ cosmos-init/plugins/packages/cosmosc2-tool-cmdsender/vue.config.js
@@ -6,33 +6,47 @@ module.exports = {
   configureWebpack: {
     devServer: {
       port: 2913,
-      watchOptions: {
-        ignored: ['node_modules'],
-        aggregateTimeout: 300,
-        poll: 1500,
-      },
       headers: {
         'Access-Control-Allow-Origin': '*',
       },
-      public: 'localhost:2913/tools/cmdsender',
+      client: {
+        webSocketURL: {
+          hostname: "0.0.0.0",
+          pathname: "/tools/cmdsender",
+          port: 2913,
+        },
+      },
+      static: {
+        watch: true,
+        // watchOptions: {
+        //   ignored: 'node_modules',
+        //   aggregateTimeout: 300,
+        //   poll: 1500,
+        // },
+      }
+      // webSocketServer: {
+      //   options: {
+      //     path: "/tools/cmdsender",
+      //   },
+      // },
     },
   },
-  chainWebpack(config) {
-    config.module
-      .rule('js')
-      .use('babel-loader')
-      .tap((options) => {
-        return {
-          rootMode: 'upward',
-        }
-      })
-    config.externals([
-      'portal-vue',
-      'vue',
-      'vuejs-dialog',
-      'vuetify',
-      'vuex',
-      'vue-router',
-    ])
-  },
+  // chainWebpack: config => {
+  //   config.module
+  //     .rule('js')
+  //     .use('babel-loader')
+  //     .tap((options) => {
+  //       return {
+  //         rootMode: 'upward',
+  //       }
+  //     })
+  //   config.externals([
+  //     'portal-vue',
+  //     'vue',
+  //     'vuejs-dialog',
+  //     'vuetify',
+  //     'vuex',
+  //     'vue-router',
+  //   ])
+  // },
 }
