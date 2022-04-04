d9a12cbb95bb8d1b740f1d8295512597703d67b8
Thomas, Jason (Software) <jmthomas@ball.com>
2022-04-04 16:15:40 -0600

    Add dialog.alert

--- cosmos-init/plugins/packages/cosmosc2-tool-autonomic/src/main.js
+++ cosmos-init/plugins/packages/cosmosc2-tool-autonomic/src/main.js
@@ -1,6 +1,5 @@
 import 'systemjs-webpack-interop/auto-public-path/2'
 import Vue from 'vue'
-import VuejsDialog from 'vuejs-dialog'
 import singleSpaVue from 'single-spa-vue'
 
 import App from './App.vue'
@@ -17,12 +16,12 @@ Vue.config.productionTip = false
 
 import '@cosmosc2/tool-common/src/assets/stylesheets/layout/layout.scss'
 import vuetify from './plugins/vuetify'
-import 'vuejs-dialog/dist/vuejs-dialog.min.css'
+import Dialog from '@cosmosc2/tool-common/src/plugins/dialog'
 import PortalVue from 'portal-vue'
 import Notify from '@cosmosc2/tool-common/src/plugins/notify'
 
 Vue.use(PortalVue)
-Vue.use(VuejsDialog)
+Vue.use(Dialog)
 Vue.use(Notify, { store })
 
 const vueLifecycles = singleSpaVue({
