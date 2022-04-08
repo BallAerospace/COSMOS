168039ef06959cc8e24871403e85db7e1b6f0aaf
van Andel, Gerhard <gvanande@ball.com>
2022-04-08 15:08:04 -0600

    add progress to processList

--- cosmos-init/plugins/packages/cosmosc2-tool-admin/src/tools/CosmosAdmin/tabs/PluginsTab.vue
+++ cosmos-init/plugins/packages/cosmosc2-tool-admin/src/tools/CosmosAdmin/tabs/PluginsTab.vue
@@ -77,8 +77,14 @@
               v-text="' Updated At: ' + formatDate(process.updated_at)"
             ></v-list-item-subtitle>
           </v-list-item-content>
-          <v-list-item-icon v-if="process.state !== 'Running'">
-            <v-tooltip bottom>
+          <v-list-item-icon>
+            <div v-if="process.state === 'Running'">
+              <v-progress-circular
+                indeterminate
+                color="primary"
+              />
+            </div>
+            <v-tooltip v-else bottom>
               <template v-slot:activator="{ on, attrs }">
                 <v-icon @click="showOutput(process)" v-bind="attrs" v-on="on">
                   mdi-eye
