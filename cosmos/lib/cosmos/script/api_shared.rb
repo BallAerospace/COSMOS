aed81c28fc5c791d173fa2a8578b562d197b033a
andyp13 <andy@phillips101.com>
2022-04-18 11:59:24 -0600

    Updating to add the exp_to_eval parameter

--- cosmos/lib/cosmos/script/api_shared.rb
+++ cosmos/lib/cosmos/script/api_shared.rb
@@ -679,7 +679,7 @@ module Cosmos
       return [target_name, packet_name, item_name, comparison_to_eval, timeout, polling_rate]
     end
 
-    def _cosmos_script_wait_implementation(target_name, packet_name, item_name, value_type, timeout, polling_rate, scope: $cosmos_scope, token: $cosmos_token, &block)
+    def _cosmos_script_wait_implementation(target_name, packet_name, item_name, value_type, timeout, polling_rate, exp_to_eval, scope: $cosmos_scope, token: $cosmos_token, &block)
       end_time = Time.now.sys + timeout
       exp_to_eval = yield
 
