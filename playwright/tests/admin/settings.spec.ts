328fb9c5ef2234ccf01639dbfd0fabd86bb4b629
Thomas, Jason (Software) <jmthomas@ball.com>
2022-04-21 21:24:17 -0600

    Finish admin settings and fix script debug

--- playwright/tests/admin/settings.spec.ts
+++ playwright/tests/admin/settings.spec.ts
@@ -29,7 +29,8 @@ test('resets clock sync warning suppression', async ({ page }) => {
     `window.localStorage['suppresswarning__clock_out_of_sync_with_server'] = true`
   )
   await page.reload()
-  await page.locator('text=Select all').click()
+  // Must force due to "subtree intercepts pointer events"
+  await page.locator('[data-test=select-all-suppressed-warnings]').click({ force: true })
   await page.locator('[data-test=reset-suppressed-warnings]').click()
   await expect(page.locator('id=cosmos-tool')).toContainText('No warnings to reset')
 })
@@ -47,57 +48,58 @@ test('clears recent configs', async ({ page }) => {
   await page.goto('/tools/admin/settings')
   await expect(page.locator('.v-app-bar')).toContainText('Administrator')
   await expect(page.locator('id=cosmos-tool')).toContainText(config)
-  await page.locator('text=Select all').click()
+  // Must force due to "subtree intercepts pointer events"
+  await page.locator('[data-test=select-all-last-configs]').click({ force: true })
   await page.locator('[data-test=clear-last-configs]').click()
   await expect(page.locator('id=cosmos-tool')).not.toContainText(config)
   localStorage = await page.evaluate(() => window.localStorage)
   expect(localStorage['lastconfig__data_viewer']).toBe(undefined)
 })
 
-test.only('sets a classification banner', async ({ page }) => {
+test('sets a classification banner', async ({ page }) => {
   await page.goto('/tools/admin/settings')
   await expect(page.locator('.v-app-bar')).toContainText('Administrator')
   await page.locator('.v-app-bar__nav-icon').click()
 
-  const bannerText = 'test classification banner'
+  const bannerText = 'Test Classification Banner'
   const bannerHeight = '32'
-  const bannerTextColor = 'aaa'
+  const bannerTextColor = 'Orange'
   const bannerBackgroundColor = '123'
   await page.locator('[data-test=classification-banner-text]').fill(bannerText)
   await page.locator('text=Display top banner').click()
   await page.locator('[data-test=classification-banner-top-height]').fill(bannerHeight)
   await page.locator('data-test=classification-banner-background-color').click()
-  await page.locator(`.v-list-item__title:text("Custom")`).click()
+  await page.locator('.v-list-item:has-text("Custom")').click()
   await page
     .locator('[data-test=classification-banner-custom-background-color]')
     .fill(bannerBackgroundColor)
   await page.locator('data-test=classification-banner-font-color').click()
-  await page.locator(`.v-list-item__title:text("Custom")`).click()
-  await page.locator('[data-test=classification-banner-custom-font-color]').fill(bannerTextColor)
+  await page.locator(`.v-list-item:has-text("${bannerTextColor}") >> nth=1`).click()
   await page.locator('[data-test=save-classification-banner]').click()
   await page.reload()
-  await page
-    .locator('#app')
-    .toHaveAttribute(
-      'style',
-      `--classification-text:"${bannerText}"; --classification-font-color:#${bannerTextColor}; --classification-background-color:#${bannerBackgroundColor}; --classification-height-top:${bannerHeight}px; --classification-height-bottom:0px;`
-    )
+  await expect(page.locator('#app')).toHaveAttribute(
+    'style',
+    `--classification-text:"${bannerText}"; --classification-font-color:${bannerTextColor.toLowerCase()}; --classification-background-color:#${bannerBackgroundColor}; --classification-height-top:${bannerHeight}px; --classification-height-bottom:0px;`
+  )
   // Disable the classification banner
   await page.locator('text=Display top banner').click()
   await page.locator('[data-test=save-classification-banner]').click()
   await page.reload()
-  await page.locator('#app').not.toHaveAttribute('style', `--classification-text:"${bannerText}"`)
+  await expect(page.locator('#app')).not.toHaveAttribute(
+    'style',
+    `--classification-text:"${bannerText}"`
+  )
 })
 
-test.only('changes the source url', async ({ page }) => {
+test('changes the source url', async ({ page }) => {
   await page.goto('/tools/admin/settings')
   await expect(page.locator('.v-app-bar')).toContainText('Administrator')
   await page.locator('.v-app-bar__nav-icon').click()
 
-  await page.locator('[data-test=source-url]').fill('https://www.space.com')
+  await page.locator('[data-test=source-url]').fill('https://www.ball.com/aerospace')
   await page.locator('[data-test=save-source-url]').click()
   await page.reload()
-  await expect(page.locator('footer a')).toHaveAttribute('href', 'https://www.space.com')
+  await expect(page.locator('footer a')).toHaveAttribute('href', 'https://www.ball.com/aerospace')
 })
 
 // TODO: Test Rubygems URL
