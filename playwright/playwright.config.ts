3db5dd3a61acd027f9627230776f08df96953c70
Thomas, Jason (Software) <jmthomas@ball.com>
2022-04-27 14:47:12 -0600

    Increase total timeout to 5 min

--- playwright/playwright.config.ts
+++ playwright/playwright.config.ts
@@ -17,7 +17,7 @@ const config: PlaywrightTestConfig = {
 
   testDir: './tests',
   /* Maximum time one test can run for. */
-  timeout: 120 * 1000,
+  timeout: 5 * 60 * 1000,
   expect: {
     /**
      * Maximum time expect() should wait for the condition to be met.
