c48953ecf82fe81954f6c1a5b47c9977d765e139
ryanmelt <ryanmelt@users.noreply.github.com>
2022-04-28 13:20:51 +0000

    [Github Action] Update version to 5.0.2-beta2

--- cosmos/lib/cosmos/version.rb
+++ cosmos/lib/cosmos/version.rb
@@ -7,7 +7,7 @@ module Cosmos
     MINOR = '0'
     PATCH = '2'
     OTHER = 'beta2'
-    BUILD = '7ed19f86fc1b782dad339c047d89583f8d09012f'
+    BUILD = '86e4fc1b82d7941b7f77e9257ecafafeff9fe3fd'
   end
   VERSION = '5.0.2-beta2'
 end
