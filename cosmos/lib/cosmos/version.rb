44f60939b28ac262caa322883fea8eab92817034
ryanatball <ryanatball@users.noreply.github.com>
2022-04-28 23:11:42 +0000

    [Github Action] Update version to 5.0.3-beta0

--- cosmos/lib/cosmos/version.rb
+++ cosmos/lib/cosmos/version.rb
@@ -1,13 +1,13 @@
 # encoding: ascii-8bit
 
-COSMOS_VERSION = '5.0.2'
+COSMOS_VERSION = '5.0.3-beta0'
 module Cosmos
   module Version
     MAJOR = '5'
     MINOR = '0'
-    PATCH = '2'
-    OTHER = ''
-    BUILD = 'ce01db297de2695bdb78b542c428ca244144d59a'
+    PATCH = '3'
+    OTHER = 'beta0'
+    BUILD = '7e5eb5362829eb143e6a3d05cf4cbb18fd362fee'
   end
-  VERSION = '5.0.2'
+  VERSION = '5.0.3-beta0'
 end
