548fc50d58722795b67fd64606422a72447239f2
Ryan Melton <ryanmelt@gmail.com>
2022-05-13 16:52:47 -0600

    bump version

--- cosmos/lib/cosmos/version.rb
+++ cosmos/lib/cosmos/version.rb
@@ -1,14 +1,14 @@
 # encoding: ascii-8bit
 
-COSMOS_VERSION = '5.0.3'
+COSMOS_VERSION = '5.0.4-beta0'
 module Cosmos
   module Version
     MAJOR = '5'
     MINOR = '0'
-    PATCH = '3'
-    OTHER = ''
-    BUILD = '715c106240ff43bb59be2d17d1f5aed50595fbc9'
+    PATCH = '4'
+    OTHER = 'pre.beta0'
+    BUILD = '80f31bc3492f4a83a5d79a28ac0a2805ec52b927'
   end
-  VERSION = '5.0.3'
-  GEM_VERSION = '5.0.3'
+  VERSION = '5.0.4-beta0'
+  GEM_VERSION = '5.0.4.pre.beta0'
 end
